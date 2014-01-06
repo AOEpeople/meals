e966b80a42877d305c1aee2b05eec15fc6e781ed
Christian Zenker <dev@xopn.de>
2014-01-06 21:53:37 +0100

    allow login

--- src/Xopn/MealzForZombies/ZombiesBundle/Entity/Zombie.php
+++ src/Xopn/MealzForZombies/ZombiesBundle/Entity/Zombie.php
@@ -3,14 +3,16 @@
 namespace Xopn\MealzForZombies\ZombiesBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * Zombie
+ * your standard website user entity
  *
  * @ORM\Table(name="zombie")
  * @ORM\Entity
  */
-class Zombie
+class Zombie implements UserInterface, \Serializable
 {
     /**
      * @var string
@@ -26,6 +28,18 @@ class Zombie
      * @var string
      */
     protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     * @var string
+     */
+    protected $salt;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     * @var string
+     */
+    protected $password;
 
     /**
      * @param string $username
@@ -59,4 +73,100 @@ class Zombie
         return $this->name;
     }
 
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->username,
+            $this->salt,
+            $this->password,
+        ));
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->username,
+            $this->salt,
+            $this->password,
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return Role[] The user roles
+     */
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // nothing to do here
+    }
 }

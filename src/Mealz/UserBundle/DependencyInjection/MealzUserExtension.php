01a8389672c8f24e9cf98f7df35718dbc903a864
Christian Zenker <dev@xopn.de>
2014-01-08 20:07:04 +0100

    fix missing classes from workspace renaming

--- src/Mealz/UserBundle/DependencyInjection/MealzUserExtension.php
+++ src/Mealz/UserBundle/DependencyInjection/MealzUserExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class XopnMealzForZombiesZombiesExtension extends Extension
+class MealzUserExtension extends Extension
 {
     /**
      * {@inheritDoc}

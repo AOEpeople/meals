111406b667a1af8bdc0b133beece6d31606041ba
Christian Zenker <dev@xopn.de>
2014-01-08 20:07:50 +0100

    show the current user if he is attending a meal

--- src/Mealz/MealBundle/DependencyInjection/Configuration.php
+++ src/Mealz/MealBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('xopn_mealz_for_zombies_mealz');
+        $rootNode = $treeBuilder->root('mealz_meal');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for

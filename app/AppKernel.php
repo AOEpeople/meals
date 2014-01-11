ccb507cf9fef488b078e723e383603f64dc604b7
Christian Zenker <dev@xopn.de>
2014-01-11 13:41:04 +0100

    add braincrafted/bootstrap-bundle
    to have form templating with bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
 			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
 			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+			// for form theming
+			new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+
 			new Mealz\MealBundle\MealzMealBundle(),
 			new Mealz\UserBundle\MealzUserBundle(),
 		);

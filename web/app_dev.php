80661e92bb57818ac1f0a129d2260acb93d0873f
Christian Zenker <christian.zenker@aoe.com>
2015-06-26 17:54:28 +0200

    disable bootstrap.php ini dev environment

--- web/app_dev.php
+++ web/app_dev.php
@@ -17,13 +17,12 @@ umask(0002);
 //    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 //}
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();

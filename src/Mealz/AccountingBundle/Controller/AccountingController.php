f76a62fba2711a6fbf82d4aa906dd88ad534de4d
John Fisher <phrix32@users.noreply.github.com>
2015-11-17 23:07:11 +0100

    [TASK] choose time period for participation listing

--- src/Mealz/AccountingBundle/Controller/AccountingController.php
+++ src/Mealz/AccountingBundle/Controller/AccountingController.php
@@ -3,20 +3,35 @@
 namespace Mealz\AccountingBundle\Controller;
 
 use Doctrine\ORM\Query;
-use Mealz\AccountingBundle\Entity\Transaction;
 use Mealz\AccountingBundle\ParticipantList\ParticipantListFactory;
 use Mealz\AccountingBundle\Service\Wallet;
 use Mealz\MealBundle\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Form\Form;
 
 class AccountingController extends BaseController
 {
+    /** @var ParticipantListFactory $participantListFactory */
+    protected $participantListFactory;
+
+    /** @var Wallet $wallet */
+    protected $wallet;
+
+    protected $formView;
+
+    /** @var \DateTime $startDay */
+    protected $startDay;
+
+    /** @var \DateTime $endDay */
+    protected $endDay;
 
     public function listAction(Request $request)
     {
+        $this->initialize($request);
+
         if ($this->getDoorman()->isKitchenStaff()) {
-            return $this->listForKitchenStaffAction($request);
+            return $this->listForKitchenStaffAction();
         } elseif ($this->get('security.context')->isGranted('ROLE_USER')) {
             return $this->listForIndividualAction();
         } else {
@@ -24,49 +39,74 @@ class AccountingController extends BaseController
         }
     }
 
-    public function listForKitchenStaffAction($request)
+    public function listForKitchenStaffAction()
     {
-        /** @var ParticipantListFactory $participantListFactory */
-        $participantListFactory = $this->get('mealz_accounting.participant_list_factory');
-
-        /** @var Wallet $wallet */
-        $wallet = $this->get('mealz_accounting.wallet');
-
-        $startDay = new \DateTime('first day of last month');
-        $endDay = new \DateTime('last day of last month');
-
-        $participantList = $participantListFactory->getList($startDay, $endDay);
+        $participantList = $this->participantListFactory->getList($this->startDay, $this->endDay);
 
         return $this->render('MealzAccountingBundle:Accounting:list_kitchen.html.twig', array(
-            'wallet' => $wallet,
-            'startDay' => $startDay,
-            'endDay' => $endDay,
-            'participantList' => $participantList
+            'wallet' => $this->wallet,
+            'startDay' => $this->startDay,
+            'endDay' => $this->endDay,
+            'participantList' => $participantList,
+            'timePeriodForm' => $this->formView
         ));
     }
 
     public function listForIndividualAction()
     {
-        /** @var ParticipantListFactory $participantListFactory */
-        $participantListFactory = $this->get('mealz_accounting.participant_list_factory');
-
-        /** @var Wallet $wallet */
-        $wallet = $this->get('mealz_accounting.wallet');
-
-        $startDay = new \DateTime('first day of last month');
-        $endDay = new \DateTime('last day of last month');
-
         $profile = $this->getProfile();
 
-        $participantList = $participantListFactory->getList($startDay, $endDay, $profile);
+        $participantList = $this->participantListFactory->getList($this->startDay, $this->endDay, $profile);
 
         return $this->render('MealzAccountingBundle:Accounting:list_individual.html.twig', array(
-            'walletBalance' => $wallet->getBalance($profile),
-            'startDay' => $startDay,
-            'endDay' => $endDay,
+            'walletBalance' => $this->wallet->getBalance($profile),
+            'startDay' => $this->startDay,
+            'endDay' => $this->endDay,
             'participations' => $participantList->getParticipations($profile),
-            'countAccountableParticipations' => $participantList->countAccountableParticipations($profile)
+            'countAccountableParticipations' => $participantList->countAccountableParticipations($profile),
+            'timePeriodForm' => $this->formView
         ));
     }
 
+    /**
+     * Initialize everything, which is used by both list actions
+     *
+     * @param Request $request
+     */
+    private function initialize(Request $request)
+    {
+        $this->participantListFactory = $this->get('mealz_accounting.participant_list_factory');
+        $this->wallet = $this->get('mealz_accounting.wallet');
+
+        $form = $this->generateTimePeriodForm();
+        $this->formView = $form->createView();
+
+        // handle form submission
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $this->startDay = $form->get('from')->getData();
+                $this->endDay = $form->get('to')->getData();
+            }
+        } else {
+            $this->startDay = new \DateTime('first day of last month');
+            $this->endDay = new \DateTime('last day of last month');
+        }
+    }
+
+    /**
+     * Generate a form where you can select a time period
+     * for the participation list
+     *
+     * @return Form
+     */
+    private function generateTimePeriodForm()
+    {
+        return $this->createFormBuilder()
+            ->add('from', 'date', array('widget' => 'single_text'))
+            ->add('to', 'date', array('widget' => 'single_text'))
+            ->add('send', 'submit')
+            ->getForm();
+    }
 }

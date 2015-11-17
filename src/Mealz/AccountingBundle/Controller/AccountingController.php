f5b50d5cbe9f36a637599d66170e9e08067b4bc0
John Fisher <phrix32@users.noreply.github.com>
2015-11-17 14:43:12 +0100

    [TASK] kitchen staff can now add cash payments choose user profile and amount in accounting view to add an transaction for the selected user

--- src/Mealz/AccountingBundle/Controller/AccountingController.php
+++ src/Mealz/AccountingBundle/Controller/AccountingController.php
@@ -3,16 +3,19 @@
 namespace Mealz\AccountingBundle\Controller;
 
 use Doctrine\ORM\Query;
+use Mealz\AccountingBundle\Entity\Transaction;
+use Mealz\AccountingBundle\Form\CashPaymentAdminForm;
 use Mealz\AccountingBundle\ParticipantList\ParticipantListFactory;
 use Mealz\AccountingBundle\Service\Wallet;
 use Mealz\MealBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class AccountingController extends BaseController {
 
-	public function listAction() {
+	public function listAction(Request $request) {
 		if ($this->getDoorman()->isKitchenStaff()) {
-			return $this->listForKitchenStaffAction();
+			return $this->listForKitchenStaffAction($request);
 		} elseif ($this->get('security.context')->isGranted('ROLE_USER')) {
 			return $this->listForIndividualAction();
 		} else {
@@ -20,7 +23,8 @@ class AccountingController extends BaseController {
 		}
 	}
 
-	public function listForKitchenStaffAction() {
+	public function listForKitchenStaffAction($request) {
+
 		/** @var ParticipantListFactory $participantListFactory */
 		$participantListFactory = $this->get('mealz_accounting.participant_list_factory');
 
@@ -32,11 +36,33 @@ class AccountingController extends BaseController {
 
 		$participantList = $participantListFactory->getList($startDay, $endDay);
 
+		$transaction = new Transaction();
+		$transaction->setId(uniqid('BAR-'));
+		$transaction->setSuccessful();
+
+		$form = $this->createForm(new CashPaymentAdminForm(), $transaction);
+
+		// handle form submission
+		if ($request->isMethod('POST')) {
+			$form->handleRequest($request);
+
+			if ($form->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($transaction);
+				$em->flush();
+
+				$this->addFlashMessage('Cash payment has been added.', 'notice');
+
+                return $this->redirectToRoute('MealzAccountingBundle_Accounting');
+			}
+		}
+
 		return $this->render('MealzAccountingBundle:Accounting:list_kitchen.html.twig', array(
 			'wallet' => $wallet,
 			'startDay' => $startDay,
 			'endDay' => $endDay,
-			'participantList' => $participantList
+			'participantList' => $participantList,
+			'form' => $form->createView()
 		));
 	}
 
---
+++ src/Mealz/AccountingBundle/Form/CashPaymentAdminForm.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mealz\AccountingBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CashPaymentAdminForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('user', 'entity', array(
+                'class' => 'MealzUserBundle:Profile',
+                'choice_label' => 'username'
+            ))
+            ->add('amount', 'money', array(
+                'scale' => 4
+            ))
+            ->add('save', 'submit');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mealz\AccountingBundle\Entity\Transaction'
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'cash';
+    }
+}

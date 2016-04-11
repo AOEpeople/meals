29b34876d64e0e08686afc7c54e7b591879edfea
Achim Rolle <info@achimrolle.de>
2016-04-11 16:39:09 +0200

    style checkboxes

--- app/Resources/js/init.js
+++ app/Resources/js/init.js
@@ -9,7 +9,48 @@ Array.prototype.remove = function () {
     return this;
 };
 
+var Mealz = function () {
+    this.checkboxWrapperClass = 'checkbox-wrapper';
+    this.$checkboxes = $('input.checkbox, input[type="checkbox"]');
+    this.$body = $('body');
+};
+
+Mealz.prototype.styleCheckboxes = function() {
+    var that = this;
+
+    this.$checkboxes.wrap('<div class="' + this.checkboxWrapperClass + '"></div>');
+
+    // Helper function to apply certain classes
+    var applyCheckboxClasses = function($checkbox) {
+        var $checkboxWrapper = $checkbox.closest('.' + that.checkboxWrapperClass);
+
+        $checkboxWrapper.toggleClass('checked', $checkbox.is(':checked'));
+        $checkboxWrapper.toggleClass('disabled', $checkbox.is(':disabled'));
+    };
+
+    // Check checkbox states
+    this.$checkboxes.each(function(idx, checkbox) {
+        var $checkbox = $(checkbox);
+        applyCheckboxClasses($checkbox);
+    });
+
+    // Handle click event on checkbox representer
+    this.$body.on('click', '.' + this.checkboxWrapperClass, function() {
+        var $checkbox = $(this).find('input');
+        $checkbox.trigger('click');
+    });
+
+    // Handle change event on checkboxes
+    this.$checkboxes.on('change', function() {
+        var $checkbox = $(this);
+        applyCheckboxClasses($checkbox);
+    });
+};
+
 $(document).ready(function() {
+
+    var mealz = new Mealz();
+    mealz.styleCheckboxes();
 
     var participations = [];
 

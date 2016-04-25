2f93ab5c6812b64c850cc301bad6a84715327df1
Achim Rolle <info@achimrolle.de>
2016-04-25 14:37:55 +0200

    new dish form reponsive

--- src/Mealz/MealBundle/Form/DishAdminForm.php
+++ src/Mealz/MealBundle/Form/DishAdminForm.php
@@ -58,7 +58,7 @@ class DishAdminForm extends AbstractType {
 				'mapped' => false
 			])
 			->add('save', SubmitType::class, array(
-				'label' => 'SAVE',
+				'label' => 'Save',
                 'attr' => [
                     'class' => 'button small'
                 ]

de864ea5eb42a9e40ce60938b0ee547d2507c608
John Fisher <phrix32@users.noreply.github.com>
2016-06-21 12:58:21 +0200

    seperate translation of column name from data model

--- src/Mealz/MealBundle/Controller/PrintController.php
+++ src/Mealz/MealBundle/Controller/PrintController.php
@@ -2,6 +2,7 @@
 
 namespace Mealz\MealBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class PrintController extends BaseController
@@ -12,29 +13,31 @@ class PrintController extends BaseController
             throw new AccessDeniedException();
         }
 
+        /** @var Translator $translator */
+        $translator = $this->get('translator');
+
         $participantRepository = $this->getParticipantRepository();
 
         $users = $participantRepository->findCostsGroupedByUserGroupedByMonth();
 
         $numberOfMonths = 3;
-        $columnNames = array('earlier');
+        $columnNames = array('earlier' => $translator->trans('costs.earlier', array(), 'general'));
         $dateTime = new \DateTime("first day of -$numberOfMonths month 00:00");
         $earlierTimestamp = $dateTime->getTimestamp();
         for ($i = 0; $i < $numberOfMonths + 1; $i++) {
-            array_push($columnNames, $dateTime->format('F'));
+            $columnNames[$dateTime->getTimestamp()] = $dateTime->format('F');
             $dateTime->modify("+1 month");
         }
-        array_push($columnNames, 'total');
+        $columnNames['total'] = $translator->trans('costs.total', array(), 'general');
 
         foreach ($users as &$user) {
-            $userCosts = array_fill_keys(array_values($columnNames), '0');
+            $userCosts = array_fill_keys(array_keys($columnNames), '0');
             foreach ($user as $cost) {
                 $monthCosts = $cost['costs'];
                 if ($cost['timestamp'] < $earlierTimestamp) {
                     $userCosts['earlier'] = bcadd($userCosts['earlier'], $monthCosts, 4);
                 } else {
-                    $monthName = date('F', $cost['timestamp']);
-                    $userCosts[$monthName] = $monthCosts;
+                    $userCosts[$cost['timestamp']] = $monthCosts;
                 }
                 $userCosts['total'] = bcadd($userCosts['total'], $monthCosts, 4);
             }

861261db047ff08d2d1b10bda427578c235282fb
John Fisher <phrix32@users.noreply.github.com>
2016-06-27 11:31:58 +0200

    fix delete dish action

--- src/Mealz/MealBundle/Controller/DishController.php
+++ src/Mealz/MealBundle/Controller/DishController.php
@@ -2,5 +2,45 @@
 
 namespace Mealz\MealBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Mealz\MealBundle\Entity\Dish;
+use Mealz\MealBundle\Entity\DishRepository;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 class DishController extends BaseListController {
+    public function deleteAction($slug)
+    {
+        if (!$this->get('security.context')->isGranted('ROLE_KITCHEN_STAFF')) {
+            throw new AccessDeniedException();
+        }
+
+        /** @var DishRepository $dishRepository */
+        $dishRepository = $this->getDoctrine()->getRepository('MealzMealBundle:Dish');
+
+        /** @var Dish $dish */
+        $dish = $dishRepository->findOneBy(array('slug' => $slug));
+
+        if (!$dish) {
+            throw $this->createNotFoundException();
+        }
+
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        if ($dishRepository->hasDishAssociatedMeals($dish)) {
+            // if there are meals assigned: just hide this record, but do not delete it
+            $dish->setEnabled(false);
+            $em->persist($dish);
+            $em->flush();
+            $this->addFlashMessage(sprintf('Dish "%s" has been hidden.', $dish->getTitle()), 'success');
+        } else {
+            // else: no need to keep an unused record
+            $em->remove($dish);
+            $em->flush();
+
+            $this->addFlashMessage(sprintf('Dish "%s" has been deleted.', $dish->getTitle()), 'success');
+        }
+
+        return $this->redirectToRoute('MealzMealBundle_Dish');
+    }
 }

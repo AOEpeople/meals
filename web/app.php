0d17414e46d9e4746121359022ad27a31b750d98
John Fisher <phrix32@users.noreply.github.com>
2016-08-02 17:52:51 +0200

    disable debugging and enable caching on prod env

--- web/app.php
+++ web/app.php
@@ -12,12 +12,12 @@ require_once __DIR__ . '/../app/AppEnvironment.php';
 $environment = AppEnvironment::fromDefault();
 $debug = $environment->getEnvironment() !== 'production';
 
-//if ($debug) {
-$loader = require_once __DIR__ . '/../app/autoload.php';
-Debug::enable();
-//} else {
-//    $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
-//}
+if ($debug) {
+    $loader = require_once __DIR__ . '/../app/autoload.php';
+    Debug::enable();
+} else {
+    $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+}
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
@@ -30,13 +30,13 @@ $apcLoader->register(true);
 */
 
 require_once __DIR__ . '/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel($environment, $debug);
 if (!$debug) {
+    require_once __DIR__.'/../app/AppCache.php';
     // @see http://symfony.com/doc/current/cookbook/debugging.html
     $kernel->loadClassCache();
+    $kernel = new AppCache($kernel);
 }
-//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();

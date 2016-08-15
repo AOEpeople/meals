c5e71eb81c809dd1c34f3c140041cf1d24f2abe3
John Fisher <phrix32@users.noreply.github.com>
2016-08-15 12:44:05 +0200

    fix setting of log folder permissions

--- setup/permissions.sh
+++ setup/permissions.sh
@@ -3,3 +3,4 @@
 echo "setting file permissions..."
 chown -R root:root ${FINAL_RELEASEFOLDER}
 chown -R www-data:www-data ${FINAL_RELEASEFOLDER}/app/cache ${FINAL_RELEASEFOLDER}/app/logs ${FINAL_RELEASEFOLDER}/web/media
+chmod -R 775 ${FINAL_RELEASEFOLDER}/app/logs

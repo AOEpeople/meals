876bd68ba32a29cac295b28a2aa7a431abdd5be6
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2016-10-21 09:57:55 +0200

    id:113354 comment: Update weeks data fixture
    Use both static and dynamically generated data. Static data is meant
    to be used with unit/functional tests. Dynamically generated data is
    meant to be used as dummy data in test environments.

--- src/Mealz/MealBundle/DataFixtures/ORM/LoadWeeks.php
+++ src/Mealz/MealBundle/DataFixtures/ORM/LoadWeeks.php
@@ -5,48 +5,74 @@ namespace Mealz\MealBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Mealz\MealBundle\Entity\Day;
-use Mealz\MealBundle\Entity\Dish;
-use Mealz\MealBundle\Entity\Meal;
 use Mealz\MealBundle\Entity\Week;
 
 
-class LoadWeeks extends AbstractFixture implements OrderedFixtureInterface {
-
-	/**
-	 * @var ObjectManager
-	 */
-	protected $objectManager;
-
+class LoadWeeks extends AbstractFixture implements OrderedFixtureInterface
+{
 	/**
 	 * @var int
 	 */
 	protected $counter = 0;
 
-	function load(ObjectManager $manager)
+	/**
+	 * @param ObjectManager $objectManager
+	 */
+	function load(ObjectManager $objectManager)
 	{
-		$this->objectManager = $manager;
+		$weeks = $this->getCurrentTestWeeks();
+		$weeks = array_merge($weeks, $this->getStaticTestWeeks());
 
-		foreach ($this->getData() as $record) {
+		foreach ($weeks as $weekDataSet) {
 			$week = new Week();
-			$week->setYear($record['year']);
-			$week->setCalendarWeek($record['calendarWeek']);
-			$this->objectManager->persist($week);
-			$this->addReference('week-' . $record['calendarWeek'], $week);
+			$week->setYear($weekDataSet['year']);
+			$week->setCalendarWeek($weekDataSet['calendarWeek']);
+			$objectManager->persist($week);
+			$this->addReference('week-' . $this->counter++, $week);
 		}
 
-		$this->objectManager->flush();
+		$objectManager->flush();
 	}
 
-	public function getData()
+	/**
+	 * Gets the current test weeks.
+	 *
+	 * They are meant to be used as dummy data on integration systems.
+	 *
+	 * @return array
+	 */
+	private function getCurrentTestWeeks()
+	{
+		$currentWeeks = [];
+		$date = new \DateTime('');
+		$maxDate = new \DateTime('+1 month');
+
+		while($date < $maxDate) {
+			$year = $date->format('Y');
+			$week = $date->format('W');
+			$currentWeeks[$year . '-' . $week] = ['year' => $year, 'calenderWeek' => $week];
+			$date->modify('+1 week');
+		}
+
+		return $currentWeeks;
+	}
+
+	/**
+	 * Gets the static test weeks.
+	 *
+	 * They are meant to be used with unit or functional tests.
+	 *
+	 * @return array
+	 */
+	private function getStaticTestWeeks()
 	{
 		return [
-			['year' => '2016', 'calendarWeek' => '41'],
-			['year' => '2016', 'calendarWeek' => '42'],
-			['year' => '2016', 'calendarWeek' => '43'],
-			['year' => '2016', 'calendarWeek' => '44'],
-			['year' => '2016', 'calendarWeek' => '45'],
-			['year' => '2016', 'calendarWeek' => '46']
+			'2016-41' => ['year' => '2016', 'calendarWeek' => '41'],
+			'2016-42' => ['year' => '2016', 'calendarWeek' => '42'],
+			'2016-43' => ['year' => '2016', 'calendarWeek' => '43'],
+			'2016-44' => ['year' => '2016', 'calendarWeek' => '44'],
+			'2016-45' => ['year' => '2016', 'calendarWeek' => '45'],
+			'2016-46' => ['year' => '2016', 'calendarWeek' => '46']
 		];
 	}
 

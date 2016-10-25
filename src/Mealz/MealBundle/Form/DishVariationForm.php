c990356d9b99b30b94d950387bb0e3ecda16003e
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2016-10-25 16:01:51 +0200

    id:113374 comment: Update dish variation form

--- src/Mealz/MealBundle/Form/DishVariationForm.php
+++ src/Mealz/MealBundle/Form/DishVariationForm.php
@@ -3,6 +3,8 @@
 namespace Mealz\MealBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,8 +19,21 @@ class DishVariationForm extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('description_de')
-			->add('description_en');
+			->add('description_de', TextType::class, [
+				'required' => TRUE,
+				'attr' => ['placeholder' => 'form.placeholder.description'],
+				'translation_domain' => 'general'
+			])
+			->add('description_en', TextType::class, [
+				'required' => TRUE,
+				'attr' => ['placeholder' => 'form.placeholder.description'],
+				'translation_domain' => 'general'
+			])
+			->add('save', SubmitType::class, [
+				'label' => 'button.save',
+				'translation_domain' => 'actions',
+				'attr' => ['class' => 'button small']
+			]);
 	}
 
 	public function configureOptions(OptionsResolver $resolver)

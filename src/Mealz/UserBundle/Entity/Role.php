fe67724b48e237b7c6f4f089e487fb6d19a4fbdc
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2016-11-09 16:47:33 +0100

    id:115089 comment:Make setters in 'Role' entity chainable

--- src/Mealz/UserBundle/Entity/Role.php
+++ src/Mealz/UserBundle/Entity/Role.php
@@ -3,126 +3,141 @@
 namespace Mealz\UserBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping AS ORM;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Role entity.
  *
- * @package Mealz\MealBundle\Entity
- * @author Chetan Thapliyal <chetan.thapliyal@aoe.com>
- *
  * @ORM\Entity
  * @ORM\Table(name="role")
  * @ORM\Entity(repositoryClass="RoleRepository")
+ *
+ * @author Chetan Thapliyal <chetan.thapliyal@aoe.com>
  */
 class Role
 {
-	/**
-	 * Constants for default roles
-	 */
-	const ROLE_KITCHEN_STAFF = 'ROLE_KITCHEN_STAFF';
-	const ROLE_USER          = 'ROLE_USER';
-	const ROLE_GUEST         = 'ROLE_GUEST';
-	const ROLE_ADMIN         = 'ROLE_ADMIN';
+    /**
+     * Constants for default roles
+     */
+    const ROLE_KITCHEN_STAFF = 'ROLE_KITCHEN_STAFF';
+    const ROLE_USER          = 'ROLE_USER';
+    const ROLE_GUEST         = 'ROLE_GUEST';
+    const ROLE_ADMIN         = 'ROLE_ADMIN';
 
 
-	/**
-	 * Role ID
-	 *
-	 * @ORM\Column(name="id", type="integer")
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="AUTO")
-	 * @var int
-	 */
-	private $id;
+    /**
+     * Role ID
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    private $id;
 
-	/**
-	 * Role name
-	 *
-	 * @ORM\Column(type="string")
-	 * @Assert\NotBlank
-	 * @var string
-	 */
-	private $title;
+    /**
+     * Role name
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     * @var string
+     */
+    private $title;
 
-	/**
-	 * Role string identifier
-	 *
-	 * @ORM\Column(type="string", unique=true)
-	 * @Assert\NotBlank
-	 * @var string
-	 */
-	private $sid;
+    /**
+     * Role string identifier
+     *
+     * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank
+     * @var string
+     */
+    private $sid;
 
-	/**
-	 * @ORM\ManyToMany(targetEntity="Profile", mappedBy="roles")
-	 * @var Collection
-	 */
-	private $profiles;
+    /**
+     * @ORM\ManyToMany(targetEntity="Profile", mappedBy="roles")
+     * @var Collection
+     */
+    private $profiles;
 
-	/**
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @param int $id
-	 */
-	public function setId($id)
-	{
-		$this->id = $id;
-	}
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
 
-	/**
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
+        return $this;
+    }
 
-	/**
-	 * @param string $title
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
-	}
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getSid()
-	{
-		return $this->sid;
-	}
+    /**
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
 
-	/**
-	 * @param string $sid
-	 */
-	public function setSid($sid)
-	{
-		$this->sid = $sid;
-	}
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSid()
+    {
+        return $this->sid;
+    }
+
+    /**
+     * @param string $sid
+     *
+     * @return $this
+     */
+    public function setSid($sid)
+    {
+        $this->sid = $sid;
+
+        return $this;
+    }
 
 
-	/**
-	 * @return Collection
-	 */
-	public function getProfiles()
-	{
-		return $this->profiles;
-	}
+    /**
+     * @return Collection
+     */
+    public function getProfiles()
+    {
+        return $this->profiles;
+    }
 
-	/**
-	 * @param Collection $profiles
-	 */
-	public function setProfiles(Collection $profiles)
-	{
-		$this->profiles = $profiles;
-	}
+    /**
+     * @param Collection $profiles
+     *
+     * @return $this
+     */
+    public function setProfiles(Collection $profiles)
+    {
+        $this->profiles = $profiles;
+
+        return $this;
+    }
 }

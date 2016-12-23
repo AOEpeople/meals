e2e5a197b7a28fce726ca004bfae8f7824271959
Simon Rininsland <simon.rininsland@aoe.com>
2016-12-23 13:44:25 +0100

    id: 120992 Abrechnungsbetrag von November: Adding 2 month

--- src/Mealz/AccountingBundle/Controller/AccountingBookController.php
+++ src/Mealz/AccountingBundle/Controller/AccountingBookController.php
@@ -21,22 +21,34 @@ class AccountingBookController extends BaseController
             throw new AccessDeniedException();
         }
 
-        // Get first and last day of previous month
-        $minDate = new \DateTime('first day of previous month');
-        $minDate->setTime(0, 0, 0);
-        $maxDate = new \DateTime('last day of previous month');
-        $maxDate->setTime(23, 59, 59);
+		// Get first and last day of previous month
+		$minDateFirst = new \DateTime('first day of previous month');
+		$minDateFirst->setTime(0, 0, 0);
+		$maxDateFirst = new \DateTime('last day of previous month');
+		$maxDateFirst->setTime(23, 59, 59);
 
-        // Create headline for twig template
-        $heading = $minDate->format('d.m.-').$maxDate->format('d.m.Y');
+		// Get first and last day of actual month
+		$minDate = new \DateTime('first day of this month');
+		$minDate->setTime(0, 0, 0);
+		$maxDate = new \DateTime('today');
+		$maxDate->setTime(23, 59, 59);
 
-        // Get array of users with their amount of transactions in previous month
-        $transactionRepository = $this->getTransactionRepository();
-        $users = $transactionRepository->findUserDataAndTransactionAmountForGivenPeriod($minDate, $maxDate);
+		// Create headline for twig template
+		$headingFirst = $minDateFirst->format('d.m.-') . $maxDateFirst->format('d.m.Y');
+		$heading = $minDate->format('d.m.-') . $maxDate->format('d.m.Y');
 
-        return $this->render('MealzAccountingBundle:Accounting\\Admin:accountingBook.html.twig', array(
-            'heading' => $heading,
-            'users' => $users,
-        ));
-    }
+		// Get array of users with their amount of transactions in previous month
+		$transactionRepository = $this->getTransactionRepository();
+		$usersFirst = $transactionRepository->findUserDataAndTransactionAmountForGivenPeriod($minDateFirst, $maxDateFirst);
+
+		// Get array of users with their amount of transactions in actual month
+		$users = $transactionRepository->findUserDataAndTransactionAmountForGivenPeriod($minDate, $maxDate);
+
+		return $this->render('MealzAccountingBundle:Accounting\\Admin:accountingBook.html.twig', array(
+			'headingFirst' => $headingFirst,
+			'heading' => $heading,
+			'usersFirst' => $usersFirst,
+			'users' => $users
+		));
+	}
 }

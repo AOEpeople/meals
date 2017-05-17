99df798c850a30602da20025e746e04a19cdc635
Joe Nathan <phrix32@users.noreply.github.com>
2017-05-17 14:30:43 +0200

    id:133565 restructure dish-selection.js

--- app/Resources/js/modules/dish-selection.js
+++ app/Resources/js/modules/dish-selection.js
@@ -1,64 +1,8 @@
 Mealz.prototype.initDishSelection = function () {
     var that = this;
 
-    $('.meal-row .dishes').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        that.selectDish($(this), e);
-    });
 
-    $('.variation-checkbox').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        that.selectVariation($(this));
-    });
-
-    // Hiding select-box and limit-box if click anywhere else
-    $('.meal-form, .meal-select-box').mouseup(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        that.hideVariationSelectBox(e);
-        var isOnSomeBox = false;
-        var limitBox = $('.limit-box');
-        limitBox.each(function(){
-            if(!isOnSomeBox){
-                isOnSomeBox = $(this).ismouseover();
-            }
-        });
-        if(!isOnSomeBox){
-            limitBox.hide();
-            limitBox.children().remove();
-        }
-    });
-
-    $('.meal-select-variations .button').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        that.hideVariationSelectBox(e);
-    });
-
-    $('.variation-button').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $(this).next().toggle();
-    });
-
-    $('.remove-meal').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        that.clearDishSelection($(this));
-        // disable remove icon
-        $(this).attr('style', 'display: none;');
-    });
-
-    $('.meal-select').on('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $('.variation-checkbox.checked', this.prev).closest('li.dishes').find('.variation-button:first').click();
-        $(this).prev().toggle();
-    });
-
-    $('.limit-icon').on('click', function (e) {
+    $('.day .limit-icon').on('click', function (e) {
         e.preventDefault();
         e.stopPropagation();
         var selectedDay = $(this).parent();
@@ -107,6 +51,64 @@ Mealz.prototype.initDishSelection = function () {
         limitBoxSelectedDay.show('fast');
     });
 
+    $('.meal-select').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.variation-checkbox.checked', this.prev).closest('li.dishes').find('.variation-button:first').click();
+        $(this).prev().toggle();
+    });
+
+    $('.meal-select .remove-meal').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that.clearDishSelection($(this));
+        // disable remove icon
+        $(this).attr('style', 'display: none;');
+    });
+
+    $('.meal-select-box .dishes').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that.selectDish($(this), e);
+    });
+
+    // Click on 'variations' button to open up variations selection
+    $('.meal-select-box .dishes .variation-button').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).next().toggle();
+    });
+
+    $('.meal-select-box .dishes .meal-select-variations .variation-checkbox').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that.selectVariation($(this));
+    });
+
+    // Click on save button in variations selection
+    $('.meal-select-variations .button').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that.hideVariationSelectBox(e);
+    });
+
+    // Hiding meal-select-box and limit-box if click anywhere else
+    $('.meal-form, .meal-select-box').mouseup(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that.hideVariationSelectBox(e);
+        var isOnSomeBox = false;
+        var limitBox = $('.limit-box');
+        limitBox.each(function(){
+            if(!isOnSomeBox){
+                isOnSomeBox = $(this).ismouseover();
+            }
+        });
+        if(!isOnSomeBox){
+            limitBox.hide();
+            limitBox.children().remove();
+        }
+    });
 };
 
 limitBoxSaveClick = function(){
@@ -132,13 +134,10 @@ Mealz.prototype.selectDish = function ($element, e) {
     var $mealRow = $element.closest('.meal-row');
     var dishId = $element.data('attribute-id');
 
-    // add remove icon
-    $mealRow.find('.remove-meal').attr('style', 'display: block;');
-
+    // If it's a dish without variations
     if ($element.attr('data-attribute-parent') !== 'true') {
         // add remove icon
         $mealRow.find('.remove-meal').attr('style', 'display: block;');
-
         $mealRow.data('attribute-selected-dish', dishId);
         $mealRow.find('.meal-label').text($element.find('.dish-title').html());
         $mealRow.attr('data-attribute-selected-variations', '');
@@ -147,7 +146,8 @@ Mealz.prototype.selectDish = function ($element, e) {
         this.createMealFormElement($mealRow, dishId);
         this.hideVariationSelectBox(e);
     }
-    // to init the participation-limit with a number
+
+    // Initialize the participation limit with 0
     if($mealRow.find('.participation-limit').val() === "" ){
         $mealRow.find('.participation-limit').val(0);
     }
@@ -158,8 +158,6 @@ Mealz.prototype.selectVariation = function ($element) {
     var variationId = $element.parent('.variation').data('attribute-id');
 
     var $mealRow = $element.closest('.meal-row');
-    var selectedDish = $mealRow.data('attribute-selected-dish');
-    var $input = $mealRow.children('.meal-selected').first();
     var variations = [];
     var that = this;
 
@@ -288,21 +286,6 @@ Mealz.prototype.hideVariationSelectBox = function (e) {
     }
 };
 
-Mealz.prototype.deleteSingleSelection = function ($element) {
-    if ($element.hasClass('meal-persisted') === true) {
-        $element.find('input:first').val('');
-    } else {
-        $element.remove();
-    }
-};
-
-Mealz.prototype.clearAllFormElements = function ($mealRow) {
-    var that = this;
-    $mealRow.find('.meal-selected').each(function () {
-        that.deleteSingleSelection($(this));
-    });
-};
-
 Mealz.prototype.createMealFormElement = function ($mealRow, dishId) {
     // Get prototype and day id and retrieve prototype form from data-prototype attribute
     if (this.prototypeFormId === undefined) {
@@ -319,4 +302,19 @@ Mealz.prototype.createMealFormElement = function ($mealRow, dishId) {
     var $prototypeFormElementInputs = $prototypeFormElement.find('input');
     $prototypeFormElementInputs.last().val(day);
     $prototypeFormElementInputs.first().val(dishId);
+};
+
+Mealz.prototype.clearAllFormElements = function ($mealRow) {
+    var that = this;
+    $mealRow.find('.meal-selected').each(function () {
+        that.deleteSingleSelection($(this));
+    });
+};
+
+Mealz.prototype.deleteSingleSelection = function ($element) {
+    if ($element.hasClass('meal-persisted') === true) {
+        $element.find('input:first').val('');
+    } else {
+        $element.remove();
+    }
 };

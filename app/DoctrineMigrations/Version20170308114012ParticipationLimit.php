57e707014f0799b8fe5c60cc2228a110fa16837a
raza.ahmed <raza.ahmed@aoe.com>
2017-12-05 10:56:24 +0100

    adapt doctrine migrations

--- app/DoctrineMigrations/Version20170308114012ParticipationLimit.php
+++ app/DoctrineMigrations/Version20170308114012ParticipationLimit.php
@@ -16,8 +16,6 @@ class Version20170308114012ParticipationLimit extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->validateDatabase();
-        $this->addSql('ALTER TABLE meal ADD participation_limit INT NOT NULL');
-        $this->addSql('ALTER TABLE participant ADD offeredAt INT NOT NULL');
     }
 
     /**
@@ -26,7 +24,5 @@ class Version20170308114012ParticipationLimit extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->validateDatabase();
-        $this->addSql('ALTER TABLE meal DROP participation_limit');
-        $this->addSql('ALTER TABLE participant DROP offeredAt');
     }
 }

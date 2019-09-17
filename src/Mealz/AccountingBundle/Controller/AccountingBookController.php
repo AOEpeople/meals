78e7d40575c51605c327fb577f1a84ef2586825b
Raza Ahmed <raza.ahmed@aoe.com>
2019-09-17 10:53:03 +0200

    [FIX] Sorry for all these stupid commits! I messed it up.

--- src/Mealz/AccountingBundle/Controller/AccountingBookController.php
+++ src/Mealz/AccountingBundle/Controller/AccountingBookController.php
@@ -1,311 +1,159 @@
 <?php
 
-namespace Mealz\AccountingBundle\Tests\Controller;
+namespace Mealz\AccountingBundle\Controller;
 
-use Mealz\AccountingBundle\Entity\Transaction;
-use Mealz\UserBundle\Entity\Profile;
-use Symfony\Component\DomCrawler\Crawler;
-use Mealz\MealBundle\Tests\Controller\AbstractControllerTestCase;
-
-use Mealz\MealBundle\DataFixtures\ORM\LoadMeals;
-use Mealz\UserBundle\DataFixtures\ORM\LoadUsers;
+use Mealz\MealBundle\Controller\BaseController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
- * Class AccountingBookControllerTest
- * @package Mealz\AccountingBundle\Tests\Controller
+ * Class AccountingBookController
+ * @package Mealz\AccountingBundle\Controller
  */
-class AccountingBookControllerTest extends AbstractControllerTestCase
+class AccountingBookController extends BaseController
 {
     /**
-     * prepare test environment
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function setUp()
+    public function listAction()
     {
-        $this->createAdminClient();
-
-        $this->clearAllTables();
-        $this->loadFixtures([
-            new LoadMeals(),
-            new LoadUsers($this->client->getContainer()),
-        ]);
-
-        $time = time();
-
-        // Create profile for user1
-        $user1FirstName = 'Max';
-        $user1LastName  = 'Mustermann'.$time;
-        $user1 = $this->createProfile($user1FirstName, $user1LastName);
-
-        // Create profile for user2
-        $user2FirstName = 'John';
-        $user2LastName  = 'Doe'.$time;
-        $user2 = $this->createProfile($user2FirstName, $user2LastName);
-
-        $this->persistAndFlushAll([$user1, $user2]);
-
-        // Create transactions for users if they're persisted
-        if (($this->getUserProfile($user1FirstName.'.'.$user1LastName) instanceof Profile) === true) {
-            $this->createTransactions($user1, 10.50, new \DateTime('first day of previous month'));
+        // Deny access for unprivileged (non-admin) users
+        if (!$this->isGranted('ROLE_KITCHEN_STAFF')) {
+            throw new AccessDeniedException();
         }
-
-        if (($this->getUserProfile($user2FirstName.'.'.$user2LastName) instanceof Profile) === true) {
-            $this->createTransactions($user2, 11.50, new \DateTime('first day of previous month'));
-        }
-    }
-
-    /**
-     * Testing access to accounting book site for non-admins and admins
-     * @test
-     */
-    public function testAccessForAdminsOnly()
-    {
-        // test for admins
-        $crawler = $this->client->request('GET', '/accounting/book');
-        $node = $crawler->filterXPath('//table[@id="accounting-book-table"]');
-        $this->assertTrue($node->count() > 0, "Accounting book NOT accessable by Admins(ROLE_KITCHEN_STAFF)");
-
-        // test for no or non-admin user
-        $this->createDefaultClient();
-        $crawler = $this->client->request('GET', '/accounting/book');
-        $node = $crawler->filterXPath('//table[@id="accounting-book-table"]');
-        $this->assertFalse($node->count() > 0, "Accounting book accessable by Non-Admins");
-    }
-
-    /**
-     * Test the headline of accounting book showing the range of the last month
-     * @test
-     */
-    public function testHeadlineShowingDateOfLastMonth()
-    {
-        // test for admins
-        $crawler = $this->client->request('GET', '/accounting/book');
-        $node = $crawler->filterXPath('//div[contains(@class,"accounting-book")]//h1[@class="headline"]');
-        $this->assertTrue($node->count() > 0, 'There is no h1.headline element)');
-
-        $headline = $node->first()->text();
 
         // Get first and last day of previous month
-        $minDate = new \DateTime('first day of previous month');
-        $maxDate = new \DateTime('last day of previous month');
+        $minDateFirst = new \DateTime('first day of previous month');
+        $minDateFirst->setTime(0, 0, 0);
+        $maxDateFirst = new \DateTime('last day of previous month');
+        $maxDateFirst->setTime(23, 59, 59);
 
-        $firstDay = $minDate->format('d');
-        $lastDay = $maxDate->format('d');
-        $monthNumber = $minDate->format('m');
-        $year = $minDate->format('Y');
-
-        $regex = "/".preg_quote($firstDay)."\.".preg_quote($monthNumber)."\.(".preg_quote($year).")? *[-|bis|to] *".preg_quote($lastDay)."\.".preg_quote($monthNumber)."\.(".preg_quote($year).")?/i";
-        $this->assertRegExp($regex, $headline, "The headline is not set properly");
-    }
-
-    /**
-     * Test if sum of all transactions for the last month is displayed in a seperate row at the end of the
-     * listed transactions.
-     * @test
-     */
-    public function testTotalAmountOfTransactionsDisplayedInSeperateRow()
-    {
-        $res = array();
-        $totalCalculated = $totalShown = 0.00;
-
-        $crawler = $this->client->request('GET', '/accounting/book');
-        $nodesAmount = $crawler->filterXPath('//table[@id="accounting-book-table"]//td[contains(@class,"amount")]');
-        $nodeTotal = $crawler->filterXPath('//table[@id="accounting-book-table"]//td[contains(@class,"table-data-total")]');
-
-        foreach ($nodesAmount as $key => $value) {
-            $tmpCrawler = new Crawler($value);
-            $res[] = floatval($tmpCrawler->text());
-        }
-        $totalCalculated = floatval(array_sum($res));
-        $totalShown = $this->getFloatFromNode($nodeTotal->siblings()->getNode(0));
-        $this->assertEquals($totalCalculated, $totalShown, 'Total amount of transactions inconsistent');
-    }
-
-    /**
-     * Test users are orderd by lastname, firstname and listed this way:
-     * lastname, firstname      amount
-     * @test
-     */
-    public function testDisplayUsersOrderedByLastnameAndFirstname()
-    {
-        // Get first and last day of previous month
-        $minDate = new \DateTime('first day of previous month');
+        // Get first and last day of actual month
+        $minDate = new \DateTime('first day of this month');
         $minDate->setTime(0, 0, 0);
-        $maxDate = new \DateTime('last day of previous month');
+        $maxDate = new \DateTime('today');
         $maxDate->setTime(23, 59, 59);
 
-        // fetch infos for previous month from database.
-        // These results are already ordered by lastname, firstname!!
-        $transactionRepository = $this->getDoctrine()->getRepository('MealzAccountingBundle:Transaction');
-        $usersAndTheirTotals = $transactionRepository->findUserDataAndTransactionAmountForGivenPeriod($minDate, $maxDate);
+        // Create headline for twig template
+        $headingFirst = $minDateFirst->format('d.m.-') . $maxDateFirst->format('d.m.Y');
+        $heading = $minDate->format('d.m.-') . $maxDate->format('d.m.Y');
 
-        // fetch what is displayed in the accounting book table....
-        $crawler = $this->client->request('GET', '/accounting/book');
-        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        // Get array of users with their amount of transactions in previous month
+        $transactionRepository = $this->getTransactionRepository();
+        $usersFirst = $transactionRepository->findUserDataAndTransactionAmountForGivenPeriod($minDateFirst, $maxDateFirst);
 
-        $nodesName = $crawler->filterXPath('//table[@id="accounting-book-table"]//td[contains(@class,"name")]');
+        // Get array of users with their amount of transactions in actual month
+        $users = $transactionRepository->findUserDataAndTransactionAmountForGivenPeriod($minDate, $maxDate);
 
-        // now compare order and displayed syntax of results
-        for ($i = 0; $i < $nodesName->count(); $i++) {
-            $this->assertTrue($nodesName->getNode($i) instanceof \DOMElement);
-            $nameDisplayed = $nodesName->getNode($i)->textContent;
-            $userInfo = current($usersAndTheirTotals);
-            next($usersAndTheirTotals);
-            $regex = "/".preg_quote($userInfo['name'])." *, *".preg_quote($userInfo['firstName'])."/i";
-            $this->assertRegExp($regex, $nameDisplayed, 'Names are displayed incorrectly. Either sorting is wrong or the names are not displayed like it should be (name, firstname)');
+        return $this->render('MealzAccountingBundle:Accounting\\Admin:accountingBook.html.twig', array(
+            'headingFirst' => $headingFirst,
+            'heading' => $heading,
+            'usersFirst' => $usersFirst,
+            'users' => $users,
+
+        ));
+    }
+
+    /**
+     * List all transactions that were payed by cash on the finances page
+     * @param $dateRange
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function listAllTransactionsAction($dateRange)
+    {
+        if (!$this->isGranted('ROLE_FINANCE')) {
+            throw new AccessDeniedException();
         }
+
+        $headingFirst = null;
+        $transactionsFirst = null;
+        $minDateFirst = null;
+        $maxDateFirst = null;
+
+        if ($dateRange === null) {
+            // Get first and last day of previous month
+            $minDateFirst = new \DateTime('first day of previous month');
+            $minDateFirst->setTime(0, 0, 0);
+            $maxDateFirst = new \DateTime('last day of previous month');
+            $maxDateFirst->setTime(23, 59, 59);
+
+            // Create headline for twig template
+            $headingFirst = $minDateFirst->format('d.m.') . ' - ' . $maxDateFirst->format('d.m.Y');
+
+            // Get first and last day of actual month
+            $minDate = new \DateTime('first day of this month');
+            $maxDate = new \DateTime('today');
+            $minDate->setTime(0, 0, 0);
+            $maxDate->setTime(23, 59, 59);
+
+            $transactionsFirst = $this->getTransactionRepository()->findAllTransactionsInDateRange($minDateFirst, $maxDateFirst);
+        } else {
+            // Get date range set with date range picker by user
+            $dateRangeArray = explode('&', $dateRange);
+            $minDate = new \DateTime($dateRangeArray[0]);
+            $maxDate = new \DateTime($dateRangeArray[1]);
+        }
+
+        $heading = $minDate->format('d.m.') . ' - ' . $maxDate->format('d.m.Y');
+
+        $transactions = $this->getTransactionRepository()->findAllTransactionsInDateRange($minDate, $maxDate);
+
+        return $this->render('MealzAccountingBundle:Accounting/Finance:finances.html.twig', array(
+            'headingFirst' => $headingFirst,
+            'heading' => $heading,
+            'transactionsFirst' => $transactionsFirst,
+            'transactions' => $transactions,
+            'minDate' => ($minDateFirst === null) ? $minDate->format('m/d/Y') : $minDateFirst->format('m/d/Y'),
+            'maxDate' => ($maxDateFirst === null) ? $maxDate->format('m/d/Y') : $maxDateFirst->format('m/d/Y'),
+        ));
     }
 
     /**
-     * Return a floatval from a node's textContent
-     * @param \DomNode $node
-     * @return float
-     */
-    protected function getFloatFromNode(\DomNode $node)
-    {
-        $res = $node->textContent;
-        $res = str_replace(',', '', $res);
-
-        return floatval($res);
-    }
-
-    /**
-     * return a new crawler
-     * @return Crawler
-     */
-    protected function getRawResponseCrawler()
-    {
-        $content = $this->client->getResponse()->getContent();
-        $uri = 'http://www.mealz.local';
-
-        return new Crawler(json_decode($content), $uri);
-    }
-
-    /**
-     * Tests if finance staff can access the transaction export page and admins and default users can not
-     * @test
-     */
-    public function testAccessForFinanceOnly() {
-        $user = $this->getUserProfile('finance');
-        $this->loginAsDefaultClient($user);
-
-        $crawler = $this->client->request('GET', '/accounting/book/finance/list');
-        $this->assertTrue($this->client->getResponse()->isSuccessful(), "Finances page not accessible by finance staff");
-
-        $node = $crawler->filterXPath('//table[@id="accounting-book-table"]');
-        $this->assertTrue($node->count() > 0, "Accounting book table could not be rendered on the finances page");
-
-        // Test if default users can access the finances page
-        $this->createDefaultClient();
-        $this->client->request('GET', '/accounting/book/finance/list');
-        $this->assertFalse($this->client->getResponse()->isSuccessful(), "Finances page accessible by default users");
-
-        // Test if admins can access the finances page
-        $this->createAdminClient();
-        $this->client->request('GET', '/accounting/book/finance/list');
-        $this->assertFalse($this->client->getResponse()->isSuccessful(), "Finances page accessible by administrators");
-    }
-
-    /**
-     * Tests if the transactions are displayed correctly
-     * @test
-     */
-    public function testTransactionsListing() {
-        $em = $this->getDoctrine()->getManager();
-        $em->getRepository('MealzAccountingBundle:Transaction')->clear();
-
-        $profile = $this->getUserProfile();
-        $transactionDate = new \DateTime('today');
-        $dateFormatted = $transactionDate->format('Y-m-d');
-
-        $transaction = new Transaction();
-        $transaction->setProfile($profile);
-        $transaction->setDate($transactionDate);
-        $transaction->setAmount(42.17);
-
-        $em->persist($transaction);
-        $em->flush();
-
-        $user = $this->getUserProfile('finance');
-        $this->loginAsDefaultClient($user);
-
-        $crawler = $this->client->request('GET', '/accounting/book/finance/list/' . $dateFormatted . "&" . $dateFormatted);
-
-        $date = $crawler->filterXPath('//*[@class="table-data date"]/text()')->getNode(0)->textContent;
-        $this->assertEquals($transactionDate->format('d.m.Y'), trim($date), "Date displayed incorrectly");
-
-        $name = $crawler->filterXPath('//*[@class="table-data name"]/text()')->getNode(0)->textContent;
-        $this->assertEquals($profile->getFullName(), trim($name), "Name displayed incorrectly");
-
-        $amount = $crawler->filterXPath('//*[@class="table-data amount"]/text()')->getNode(0)->textContent;
-        $this->assertEquals("42.17", trim($amount), "Amount displayed incorrectly");
-    }
-
-    /**
-     * Test if PayPal payments are shown on the finances page
-     * @test
+     * Export transaction table as PDF for finance staff
+     * @param $dateRange
+     * @return string
      * @throws \Exception
      */
-    public function testOnlyCashPaymentsListed() {
-        $em = $this->getDoctrine()->getManager();
-        $em->getRepository('MealzAccountingBundle:Transaction')->clear();
+    public function exportPDFAction($dateRange)
+    {
+        if (!$this->isGranted('ROLE_FINANCE')) {
+            throw new AccessDeniedException();
+        }
 
-        $profile = $this->getUserProfile();
-        $transactionDate = new \DateTime('today');
-        $dateFormatted = $transactionDate->format('Y-m-d');
+        // Get date range set with date range picker by user
+        $dateRange = str_replace('-', '/', $dateRange);
+        $dateRangeArray = explode('&', $dateRange);
+        $minDate = new \DateTime($dateRangeArray[0]);
+        $maxDate = new \DateTime($dateRangeArray[1]);
 
-        $transaction = new Transaction();
-        $transaction->setProfile($profile);
-        $transaction->setDate($transactionDate);
-        $transaction->setAmount(42.17);
-        $transaction->setPaymethod(0);
+        $heading = $minDate->format('d.m.') . ' - ' . $maxDate->format('d.m.Y');
+        $transactionRepository = $this->getTransactionRepository();
+        $transactions = $transactionRepository->findAllTransactionsInDateRange($minDate, $maxDate);
 
-        $em->persist($transaction);
-        $em->flush();
+        // Create PDF file
+        $pdf = $this->get('white_october.tcpdf')->create(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $pdf->setHeaderData('', 0, '', '', array(0, 0, 0), array(255, 255, 255));
+        $pdf->setPrintHeader(false);
+        $pdf->setPrintFooter(false);
+        $pdf->AddPage();
 
-        $user = $this->getUserProfile('finance');
-        $this->loginAsDefaultClient($user);
+        $filename = $this->get('translator')->trans('payment.transaction_history.finances.pdf') . '-' . $minDate->format('d.m.Y') . '-' . $maxDate->format('d.m.Y');
+        $pdf->SetTitle($filename);
 
-        $crawler = $this->client->request('GET', '/accounting/book/finance/list/' . $dateFormatted . "&" . $dateFormatted);
+        $cssFile = file_get_contents($this->getParameter('env_url') . '/media/transaction-export.css');
 
-        $nodes = $crawler->filterXPath('//*[@class="table-data amount"]/text()');
-        $this->assertEquals(0, $nodes->count(), "PayPal payment listed on finances page");
+        $includeCSS = '<style>' . $cssFile . '</style>';
+
+        $html = $this->renderView('MealzAccountingBundle:Accounting/Finance:print_finances.html.twig', array(
+            'headingFirst' => null,
+            'heading' => $heading,
+            'transactionsFirst' => null,
+            'transactions' => $transactions,
+            'minDate' => $minDate->format('m/d/Y'),
+            'maxDate' => $maxDate->format('m/d/Y'),
+        ));
+
+        $pdf->writeHTML($includeCSS . $html);
+        return $pdf->Output($filename . '.pdf', 'I');
     }
-
-    /**
-     * Tests if the daily closing amount is calculated correctly
-     * @test
-     * @throws \Exception
-     */
-    public function testDailyClosingCalculation() {
-        $em = $this->getDoctrine()->getManager();
-        $em->getRepository('MealzAccountingBundle:Transaction')->clear();
-
-        $profile = $this->getUserProfile();
-        $transactionDate = new \DateTime('today');
-        $dateFormatted = $transactionDate->format('Y-m-d');
-
-        $transaction = new Transaction();
-        $transaction->setProfile($profile);
-        $transaction->setDate($transactionDate->setTime(12, 00, 00));
-        $transaction->setAmount(42.17);
-
-        $em->persist($transaction);
-
-        $transaction = new Transaction();
-        $transaction->setProfile($profile);
-        $transaction->setDate($transactionDate->setTime(13, 00, 00));
-        $transaction->setAmount(57.83);
-
-        $em->persist($transaction);
-        $em->flush();
-
-        $user = $this->getUserProfile('finance');
-        $this->loginAsDefaultClient($user);
-
-        $crawler = $this->client->request('GET', '/accounting/book/finance/list/' . $dateFormatted . "&" . $dateFormatted);
-
-        $dailyClosing = $crawler->filterXPath('//*[@class="table-data daily-closing"]/text()')->getNode(0)->textContent;
-        $this->assertEquals("100.00", trim($dailyClosing), "Daily closing calculated incorrectly");
-    }
-
 }

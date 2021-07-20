99f9da10542ebf74ca0fb94875cfdfc7442f4b75
daniel.motzko <daniel.motzko@aoe.com>
2021-07-20 13:18:06 +0200

    Add spec for reservation functionality

--- tests/e2e/cypress/integration/meals.aoe.com/reservation.function.spec.ts
+++ tests/e2e/cypress/integration/meals.aoe.com/reservation.function.spec.ts
@@ -1,7 +1,16 @@
 import * as data from "../../fixtures/data.json";
 
-describe.skip("reservation.function", () => {
+describe("reservation.function", () => {
   const checkComponent = (user: String) => {
+    let participantsAfterJoinAction: number;
+    let participantsAfterDeleteAction: number;
+
+    // intercept the join request aka "the reservation"
+    cy.intercept("GET", "**/join").as("getJoin");
+
+    // intercept the delete request aka "the cancellation"
+    cy.intercept("GET", "**/delete").as("getDelete");
+
     // log user in
     cy.get("header input[name='_username']").type(`${user}`);
     cy.get("header input[name='_password']").type(`${user}`);
@@ -13,26 +22,52 @@ describe.skip("reservation.function", () => {
     // at least one meal should be available
     cy.get("[class='meal is-available']")
       .find("input[class*='join-action']")
+      .as("joinAction")
       .should("have.length.at.least", 1);
 
-    // https://docs.cypress.io/api/commands/intercept#Request-Response-Modification-with-routeHandler
+    cy.get("@joinAction").first().click({ force: true });
 
-    cy.get("[class='meal is-available']")
-      .find("input[class*='join-action']")
-      .first()
-      .click({ force: true })
-      .invoke("val")
-      .then((val) => {
+    cy.wait("@getJoin").then(({ response }) => {
+      expect(response && response.body).to.have.property("participantsCount");
+      expect(response && response.body).to.have.property("url");
+
+      if (response) {
+        participantsAfterJoinAction = response.body.participantsCount;
         cy.reload();
-        //if (val != undefined) let val2 = val.replace("/join", "/delete");
-        //cy.log("CCC " + val2);
-        //cy.get(`input[value='${val2}']`).should("be.visible")
+        cy.get(`input[value='${response.body.url}']`)
+          .as("deleteAction")
+          .should("exist");
+      }
+
+      cy.get("@deleteAction").click({ force: true });
+
+      cy.wait("@getDelete").then(({ response }) => {
+        expect(response && response.body).to.have.property("participantsCount");
+        expect(response && response.body).to.have.property("url");
+
+        if (response) {
+          participantsAfterDeleteAction = response.body.participantsCount;
+          expect(participantsAfterDeleteAction).eq(
+            participantsAfterJoinAction - 1
+          );
+          cy.get(`input[value='${response.body.url}']`).should("exist");
+        }
+
+        cy.reload();
+        cy.get("@joinAction").should("exist");
       });
+    });
   };
 
   it("is working fine in viewport 'desktop'", () => {
     cy.visitMeals();
     cy.viewportXL();
     checkComponent(data.user.bob);
+  });
+
+  it("is working fine in viewport 'desktop'", () => {
+    cy.visitMeals();
+    cy.viewportXL();
+    checkComponent(data.user.kochomi);
   });
 });

38ef1de83546c38ef785344d234daf1b17c44c0e
daniel.motzko <daniel.motzko@aoe.com>
2021-08-09 11:33:49 +0200

    Set locale to 'en'

--- tests/e2e/cypress/support/commands/login.ts
+++ tests/e2e/cypress/support/commands/login.ts
@@ -1,4 +1,7 @@
 export const login = (user: string) => {
+  // set cookie 'locale' to 'en'
+  cy.setCookie("locale", "en");
+
   // check visibility of elements
   cy.get("header input[name='_username']").should("be.visible").as("name");
   cy.get("header input[name='_password']").should("be.visible").as("password");

96c4e6a50dbe38023796a405362b640ef647170f
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2021-10-27 09:18:51 +0200

    #249136 Add Slot controller

---
+++ src/Mealz/MealBundle/Controller/SlotController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mealz\MealBundle\Controller;
+
+use App\Mealz\MealBundle\Entity\Slot;
+use App\Mealz\MealBundle\Service\SlotService;
+use Exception;
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class SlotController extends BaseListController
+{
+    public function update(Request $request, SlotService $slotService, Slot $slot): JsonResponse
+    {
+        if ('POST' !== $request->getMethod()) {
+            return new JsonResponse(null, 405);
+        }
+
+        $data = $request->request->all();
+
+        try {
+            $slotService->update($slot, $data);
+        } catch (InvalidArgumentException $e) {
+            return new JsonResponse(null, 422);
+        } catch (Exception $e) {
+            $this->logException($e);
+
+            return new JsonResponse(null, 500);
+        }
+
+        return new JsonResponse(['status' => 'success']);
+    }
+}

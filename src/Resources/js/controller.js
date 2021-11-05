d0fd793fb250b98f4114cff62bed27c78f61fe6c
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2021-11-05 13:32:37 +0100

    #249457 Remove debug code in controller.js

--- src/Resources/js/controller.js
+++ src/Resources/js/controller.js
@@ -1,7 +1,5 @@
 let Controller = function (view) {
-    console.log('Initializing controller...');
     if (typeof view == 'string' && view !== '') {
-        console.log('Loading view: "'+view+'" ...')
         this.getView(view);
     }
 };
@@ -9,14 +7,8 @@ let Controller = function (view) {
 Controller.prototype.getView = function (viewName) {
     const viewFile = './'+viewName+'.js';
     const context = require.context('./views', false, /.*\.js$/);
-
-    // try {
-        const viewObj = context(viewFile);
-        console.log(viewObj);
-        new viewObj.default();
-    // } catch (err) {
-    //     console.log(err);
-    // }
+    const viewObj = context(viewFile);
+    new viewObj.default();
 };
 
 export { Controller };

6ac645d71887e49bb8c098c5dbab54505feee8d8
Thomas Gietzen <thomas.gietzen@aoe.com>
2021-11-08 12:36:02 +0100

    #249390 Extend UI with buttons for hiding users and toggle the hidden users. Add styles and implement JS logic for the new actions.

---
+++ src/Resources/js/modules/toggle-hidden-users.js
@@ -0,0 +1,44 @@
+import Switchery from 'switchery.js';
+
+Mealz.prototype.initHiddenUsersToggler = function () {
+    const self = this;
+    let checkbox = document.querySelector('#toggle-hidden-users')
+    if (checkbox === null)
+        return
+
+    let state = checkbox.checked
+
+    // reload cached state
+    let showHiddenUsers = localStorage.getItem('showHiddenUsers')
+    if (showHiddenUsers !== null) {
+        let cachedState = showHiddenUsers === 'true'
+        if (state !== cachedState) {
+            state = cachedState
+            if (cachedState) {
+                checkbox.setAttribute('checked', 'checked')
+            }
+            else {
+                checkbox.removeAttribute('checked')
+            }
+        }
+    }
+
+    self.toggleHiddenUsers(state)
+
+    new Switchery(checkbox, {
+        size: 'small',
+        onChange: function (state) {
+            self.toggleHiddenUsersChangeHandler(state);
+        }
+    });
+};
+
+Mealz.prototype.toggleHiddenUsersChangeHandler = function (state) {
+    Mealz.prototype.toggleHiddenUsers(state)
+    localStorage.setItem('showHiddenUsers', state)
+};
+
+Mealz.prototype.toggleHiddenUsers = function(state) {
+    let userRow = $('.user-hidden')
+    state ? userRow.show() : userRow.hide()
+}

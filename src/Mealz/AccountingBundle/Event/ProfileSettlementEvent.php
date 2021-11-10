ee3405ef2a29cd3bc37fdfc36d6f1a77f976630e
Clemens Queissner <clemens.queissner@cq-design.de>
2021-11-10 11:11:22 +0100

    #249382: Added event and subscriber to be able to dispatch custom event

---
+++ src/Mealz/AccountingBundle/Event/ProfileSettlementEvent.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Mealz\AccountingBundle\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+use App\Mealz\UserBundle\Entity\Profile;
+
+class ProfileSettlementEvent extends Event
+{
+    public const NAME = 'profile.settlement.confirm';
+
+    /**
+     * @var Profile
+     */
+    protected Profile $profile;
+
+    public function __construct(Profile $profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /**
+     * @return Profile
+     */
+    public function getProfile(): Profile
+    {
+        return $this->profile;
+    }
+}
---
+++ src/Mealz/AccountingBundle/Event/Subscriber/SettlementSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mealz\AccountingBundle\Event\Subscriber;
+
+use App\Mealz\AccountingBundle\Event\ProfileSettlementEvent;
+use App\Mealz\MealBundle\Entity\ParticipantRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class SettlementSubscriber implements EventSubscriberInterface
+{
+
+    private ParticipantRepository $participantRepo;
+
+    public function __construct(ParticipantRepository $participantRepo){
+        $this->participantRepo = $participantRepo;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProfileSettlementEvent::NAME => 'onProfileSettlement'
+        ];
+    }
+
+    /**
+     * @param ProfileSettlementEvent $event
+     */
+    public function onProfileSettlement(ProfileSettlementEvent $event) {
+        $this->participantRepo->removeFutureMealsByProfile($event->getProfile());
+    }
+}

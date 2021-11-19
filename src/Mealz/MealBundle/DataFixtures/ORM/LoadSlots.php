1fe2f3673e250cd7af394f16db8ecc5ff8bf8d7b
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2021-11-19 11:49:12 +0100

    #249004 [update] Meal slots - admin mgmt.
    * #249608 Allow admin and kitchen staff to delete slots
    * #249606 Fix listing order of participants in print view
    * #249609 Rename slot item in main navigation

---
+++ src/Mealz/MealBundle/DataFixtures/ORM/LoadSlots.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mealz\MealBundle\DataFixtures\ORM;
+
+use App\Mealz\MealBundle\Entity\Slot;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * Fixtures Load the Categories
+ */
+class LoadSlots extends Fixture implements OrderedFixtureInterface
+{
+    /**
+     * Constant to declare load order of fixture
+     */
+    private const ORDER_NUMBER = 4;
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $slotItems = [
+            [
+                'title' => 'Active w/o limit',
+                'limit' => 0,
+                'deleted' => false,
+                'disabled' => false,
+                'slug' => 'active-wo-limit'
+            ],
+            [
+                'title' => 'Active w/ limit',
+                'limit' => 10,
+                'deleted' => false,
+                'disabled' => false,
+                'slug' => 'active-w-limit'
+            ],
+            [
+                'title' => 'Inactive',
+                'limit' => 0,
+                'deleted' => false,
+                'disabled' => true,
+                'slug' => 'inactive'
+            ],
+            [
+                'title' => 'Deleted',
+                'limit' => 0,
+                'deleted' => true,
+                'disabled' => false,
+                'slug' => 'deleted'
+            ],
+        ];
+
+        foreach ($slotItems as $item) {
+            $slot = new Slot();
+            $slot->setTitle($item['title']);
+            $slot->setLimit($item['limit']);
+            $slot->setDeleted($item['deleted']);
+            $slot->setDisabled($item['disabled']);
+            $slot->setSlug($item['slug']);
+
+            $manager->persist($slot);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrder(): int
+    {
+        return self::ORDER_NUMBER;
+    }
+}

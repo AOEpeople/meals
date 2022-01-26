e5c824460bf87362d81563cfa5161784b114c54c
Thomas Gietzen <thomas.gietzen@aoe.com>
2022-01-26 12:24:28 +0100

    #249695 Adds DishTest with is combined dish test.

---
+++ src/Mealz/MealBundle/Tests/Entity/DishTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mealz\MealBundle\Tests\Entity;
+
+use App\Mealz\MealBundle\Entity\Dish;
+use App\Mealz\MealBundle\Tests\AbstractDatabaseTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+
+class DishTest extends AbstractDatabaseTestCase
+{
+    private EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->clearAllTables();
+
+        /* @var EntityManagerInterface $entityManager */
+        $this->entityManager = $this->getDoctrine()->getManager();
+    }
+
+    /**
+     * @test
+     */
+    public function isNotCombinedDish(): void
+    {
+        $dish = new Dish();
+        $dish->setTitleEn("Some tasty Dish");
+        $dish->setTitleDe("Ein schmackhaftes Gericht");
+
+        $this->entityManager->persist($dish);
+        $this->entityManager->flush();
+        $this->entityManager->refresh($dish);
+
+        $this->assertNotEmpty($dish->getSlug());
+        $this->assertFalse($dish->isCombinedDish());
+    }
+
+    /**
+     * @test
+     */
+    public function isCombinedDish(): void
+    {
+        $dish = new Dish();
+        $dish->setTitleEn("Combined Dish");
+        $dish->setTitleDe("Kombi Gericht");
+
+        $this->entityManager->persist($dish);
+        $this->entityManager->flush();
+        $this->entityManager->refresh($dish);
+
+        $this->assertNotEmpty($dish->getSlug());
+        $this->assertEquals(Dish::COMBINED_DISH_SLUG, $dish->getSlug());
+        $this->assertTrue($dish->isCombinedDish());
+    }
+}

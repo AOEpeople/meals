b3bec04d6ab18e84a660d7512b9ab2ace427ed07
Thomas Gietzen <thomas.gietzen@aoe.com>
2022-01-26 16:07:35 +0100

    #249695 Adds test for getter and setter of combined dishes.

---
+++ src/Mealz/MealBundle/Tests/Entity/ParticipantTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mealz\MealBundle\Tests\Entity;
+
+use App\Mealz\MealBundle\Entity\Dish;
+use App\Mealz\MealBundle\Entity\DishCollection;
+use App\Mealz\MealBundle\Entity\Meal;
+use App\Mealz\MealBundle\Entity\Participant;
+use App\Mealz\MealBundle\Tests\AbstractDatabaseTestCase;
+use App\Mealz\UserBundle\Entity\Profile;
+
+class ParticipantTest extends AbstractDatabaseTestCase
+{
+    private Participant $participant;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $meal = new Meal();
+        $profile = new Profile();
+
+        $this->participant = new Participant($profile, $meal);
+    }
+
+    /**
+     * @test
+     */
+    public function combinedDishesIsEmptyInitially(): void
+    {
+        $this->assertEmpty($this->participant->getCombinedDishes());
+    }
+
+    /**
+     * @test
+     */
+    public function combinedDishesIsEmptyWhenSetToNull(): void
+    {
+        $this->participant->setCombinedDishes(null);
+        $this->assertEmpty($this->participant->getCombinedDishes());
+    }
+
+    /**
+     * @test
+     */
+    public function combinedDishesIsEmptyForEmptyDishCollection(): void
+    {
+        $this->participant->setCombinedDishes(new DishCollection());
+        $this->assertEmpty($this->participant->getCombinedDishes());
+    }
+
+    /**
+     * @test
+     */
+    public function combinedDishesIsACopy(): void
+    {
+        $dishCollection = new DishCollection([
+            new Dish(),
+            new Dish(),
+            new Dish(),
+        ]);
+
+        $this->participant->setCombinedDishes($dishCollection);
+
+        $dishCollection->remove(0);
+        $this->assertCount(2, $dishCollection);
+        $this->assertCount(3, $this->participant->getCombinedDishes());
+
+        $dishCollection->add(new Dish());
+        $this->assertCount(3, $dishCollection);
+        $this->assertCount(3, $this->participant->getCombinedDishes());
+
+        $this->participant->getCombinedDishes()->clear();
+        $this->assertCount(3, $this->participant->getCombinedDishes());
+
+        $this->participant->getCombinedDishes()->add(new Dish());
+        $this->assertCount(3, $this->participant->getCombinedDishes());
+
+        $this->participant->getCombinedDishes()->remove(2);
+        $this->assertCount(3, $this->participant->getCombinedDishes());
+    }
+
+}

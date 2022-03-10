f8a03be5c475d0b68e0095a8c41912b89e056a63
Martin Tepper <martin.tepper@aoe.com>
2022-03-10 10:25:47 +0100

    251898: Guestlink öffnet nicht

--- src/Resources/js/modules/custom-functions.js
+++ src/Resources/js/modules/custom-functions.js
@@ -13,47 +13,54 @@ Mealz.prototype.copyToClipboard = function() {
 
     // click events
     $('.guest-menu').on('click', function () {
+        var $that = $(this);
+
         // Close all open overlays
         $('.guest-menu-link').removeClass('open');
 
-        var dayId = $(this).attr('data-copytarget').split('-').pop();
-        var guestMenuLinkInput = $(this).parent().find('.guest-menu-link textarea');
+        var dayId = $that.attr('data-copytarget').split('-').pop();
+        var guestMenuLinkInput = $that.parent().find('.guest-menu-link textarea');
 
-        var result = Mealz.prototype.loadGeneratedLink(dayId);
-        if (that.isUrl(result)) {
-            guestMenuLinkInput.val(result);
-            $(this).next().addClass('open');
-            if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
-                var el = guestMenuLinkInput.get(0);
-                var editable = el.contentEditable;
-                var readOnly = el.readOnly;
-                el.contentEditable = true;
-                el.readOnly = false;
-                var range = document.createRange();
-                range.selectNodeContents(el);
-                var sel = window.getSelection();
-                sel.removeAllRanges();
-                sel.addRange(range);
-                el.setSelectionRange(0, 999999);
-                el.contentEditable = editable;
-                el.readOnly = readOnly;
-            } else {
-                guestMenuLinkInput.trigger('select');
+        $.ajax({
+            method: 'GET',
+            url: '/menu/' + dayId + '/new-guest-invitation',
+            success: function (result) {
+                if (that.isUrl(result)) {
+                    guestMenuLinkInput.val(result);
+                    $that.next().addClass('open');
+                    if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+                        var el = guestMenuLinkInput.get(0);
+                        var editable = el.contentEditable;
+                        var readOnly = el.readOnly;
+                        el.contentEditable = true;
+                        el.readOnly = false;
+                        var range = document.createRange();
+                        range.selectNodeContents(el);
+                        var sel = window.getSelection();
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                        el.setSelectionRange(0, 999999);
+                        el.contentEditable = editable;
+                        el.readOnly = readOnly;
+                    } else {
+                        guestMenuLinkInput.trigger('select');
+                    }
+                    // Clipboard copy only works in secure context, redirect http -> https
+                    if (window.location.protocol === 'https:') {
+                        navigator.clipboard.writeText(result);
+                    } else {
+                        console.warn('Clipboard access from insecure (HTTP) context is prohibited. Reload page with HTTPS and try again.');
+                    }
+                    guestMenuLinkInput.trigger('blur');
+                } else {
+                    var $html = $(result);
+                    if ($html.length && $html.find('.login-form').length) {
+                        window.location.reload();
+                    }
+                }
+                return false;
             }
-            // Clipboard copy only works in secure context, redirect http -> https
-            if (window.location.protocol === 'https:') {
-                navigator.clipboard.writeText(result);
-            } else {
-                console.warn('Clipboard access from insecure (HTTP) context is prohibited. Reload page with HTTPS and try again.');
-            }
-            guestMenuLinkInput.trigger('blur');
-        } else {
-            var $html = $(result);
-            if ($html.length && $html.find('.login-form').length) {
-                window.location.reload();
-            }
-        }
-        return false;
+        });
     });
 
     $(document).on('click', function (event) {
@@ -66,19 +73,4 @@ Mealz.prototype.copyToClipboard = function() {
 Mealz.prototype.isUrl = function(s) {
     var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
     return regexp.test(s);
-};
-
-Mealz.prototype.loadGeneratedLink = function(dayId) {
-    'use strict';
-
-    var result = null;
-
-    $.ajax({
-        method: 'GET',
-        url: '/menu/' + dayId + '/new-guest-invitation',
-        success: function (data) {
-            result = data;
-        }
-    });
-    return result;
 };

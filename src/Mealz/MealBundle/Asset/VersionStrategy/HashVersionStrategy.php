a69ec0db732b43621250a70e040698005ea7e040
Martin Tepper <martin.tepper@aoe.com>
2022-03-31 11:23:47 +0200

    252243: add hash to assets for cache detection after deployment

---
+++ src/Mealz/MealBundle/Asset/VersionStrategy/HashVersionStrategy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Mealz\MealBundle\Asset\VersionStrategy;
+
+use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
+
+class HashVersionStrategy implements VersionStrategyInterface
+{
+    public function getVersion($path)
+    {
+        return substr(md5_file($path), 0, 7);
+    }
+
+    public function applyVersion($path)
+    {
+        return sprintf('%s?v=%s', $path, $this->getVersion($path));
+    }
+}

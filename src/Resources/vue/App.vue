545d68cef0e642315287fedaa8dd6a69754eff70
Simon Henz <simon.henz@aoe.com>
2022-05-06 14:43:31 +0200

    #252664 installed Vue3 added watch functionality to frontend build

---
+++ src/Resources/vue/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="../images/vue.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Test
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

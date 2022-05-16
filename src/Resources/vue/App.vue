6a4034230e8c65895a513b52777adac77843a64e
Simon Henz <simon.henz@aoe.com>
2022-05-16 11:32:27 +0200

    started replacing frontend with vue @ /test

--- src/Resources/vue/App.vue
+++ src/Resources/vue/App.vue
@@ -1,27 +1,10 @@
 <template>
-  <img alt="Vue logo" src="../images/vue.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  Test
+  <MainView />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import MainView from "./view/MainView.vue";
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

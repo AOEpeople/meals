9d47c33f187b8f8ce237cc007a15b15012b67af7
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2022-05-18 13:25:52 +0200

    Fix PHP-CS and PHP-MD issues

--- src/Mealz/MealBundle/Service/Logger/MealsLogger.php
+++ src/Mealz/MealBundle/Service/Logger/MealsLogger.php
@@ -7,6 +7,9 @@ namespace App\Mealz\MealBundle\Service\Logger;
 use Psr\Log\LoggerInterface;
 use Throwable;
 
+/**
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ */
 class MealsLogger implements MealsLoggerInterface
 {
     private LoggerInterface $logger;
@@ -16,47 +19,47 @@ class MealsLogger implements MealsLoggerInterface
         $this->logger = $logger;
     }
 
-    public function emergency($message, array $context = array()): void
+    public function emergency($message, array $context = []): void
     {
         $this->logger->emergency($message, $context);
     }
 
-    public function alert($message, array $context = array()): void
+    public function alert($message, array $context = []): void
     {
         $this->logger->alert($message, $context);
     }
 
-    public function critical($message, array $context = array()): void
+    public function critical($message, array $context = []): void
     {
         $this->logger->critical($message, $context);
     }
 
-    public function error($message, array $context = array()): void
+    public function error($message, array $context = []): void
     {
         $this->logger->error($message, $context);
     }
 
-    public function warning($message, array $context = array()): void
+    public function warning($message, array $context = []): void
     {
         $this->logger->warning($message, $context);
     }
 
-    public function notice($message, array $context = array()): void
+    public function notice($message, array $context = []): void
     {
         $this->logger->notice($message, $context);
     }
 
-    public function info($message, array $context = array()): void
+    public function info($message, array $context = []): void
     {
         $this->logger->info($message, $context);
     }
 
-    public function debug($message, array $context = array()): void
+    public function debug($message, array $context = []): void
     {
         $this->logger->debug($message, $context);
     }
 
-    public function log($level, $message, array $context = array()): void
+    public function log($level, $message, array $context = []): void
     {
         $this->logger->log($level, $message, $context);
     }

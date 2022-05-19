0f2732aacbc52c9f9e20aa74b68f5821bfdac3aa
Simon Henz <simon.henz@aoe.com>
2022-05-19 10:57:32 +0200

    implemented aoe-group-web-cd

--- src/Resources/vue/components/Footer.vue
+++ src/Resources/vue/components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
   <footer class="bg-[rgb(225,232,238)]">
-    <div class="max-w-[1170] mx-auto flex">
-      <div class="mx-20 p-4 w-1/2 flex-nowrap justify-center">
-        <img class="h-10" src="../../../../public/images/aoe-logo.svg" alt="AOE Logo" />
+    <div class="grid mx-auto max-w-screen-aoe xl:grid-cols-2">
+      <div class="items-center py-4">
+        <img class="mx-auto h-10 xl:mx-0" src="../../../../public/images/aoe-logo.svg" alt="AOE Logo" />
       </div>
-      <div class="w-1/2">
-        <a class="w-[50%] cursor-pointer hover:text-[#FF890E] justify-end self-center">
-          <img class="max-w-[5%] w-4 mr-2" src="../../../../public/images/flag.svg" alt="flag" />
+      <div class="hidden self-center text-right xl:inline-block">
+        <img class="inline-block mr-1 w-5" src="../../../../public/images/flag.svg" alt="flag" />
+        <a class="self-center cursor-pointer hover:text-highlight">
           {{ lang }} version
         </a>
       </div>
     </div>
-    <div class="bg-[rgb(244,247,249)] drop-shadow-[0_1px_2px_rgba(0,0,0,0.1)] w-full py-4 px-4 text-center">
+    <div class="bg-[rgb(244,247,249)] w-full py-4 px-4 text-center">
       <a> @ 2022 AOE. All rights reserved </a>
     </div>
   </footer>

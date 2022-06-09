7161b15c95cc06ac19b29b3f318527ed30a4b866
Simon Henz <simon.henz@aoe.com>
2022-06-09 09:38:00 +0200

    added components

---
+++ src/Resources/vue/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <NavBar />
+  <Content />
+  <Footer />
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+import Content from "@/components/Content.vue";
+
+</script>

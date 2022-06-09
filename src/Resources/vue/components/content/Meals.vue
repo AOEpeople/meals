92b45f5e735dfe1d57a5aeb20931a5a992adc744
Simon Henz <simon.henz@aoe.com>
2022-06-09 09:38:00 +0200

    added components

---
+++ src/Resources/vue/components/content/Meals.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="text-center">
+    <h1 class="m-0">Current Week</h1>
+    <p class="description text-primary">Mon 04.10. - Fri 08.10.</p>
+  </div>
+  <div class="grid gap-y-10" id="weekly-menu">
+    <Day v-for="day in week"
+         class="mx-auto"
+         :day="day"
+         :key="day.name"
+    />
+  </div>
+</template>
+
+<script setup>
+import Day from './Day.vue'
+
+var meal1 = {
+  name: 'Meal1',
+  description: '',
+  participations: 42,
+  active: false,
+}
+
+var meal2 = {
+  name: 'Meal2',
+  description: 'testing',
+  participations: 25,
+  active: true,
+}
+
+var kombi = {
+  name: 'Kombi',
+  description: '',
+  participations: 3,
+  active: false,
+}
+
+var monday = {
+  name: 'Monday',
+  meal1: meal1,
+  meal2: meal2,
+  kombi: kombi,
+}
+var thuesday = {
+  name: 'Thuesday',
+  meal1: meal1,
+  meal2: meal2,
+  kombi: kombi,
+}
+var wednesday = {
+  name: 'Wednesday',
+  meal1: meal1,
+  meal2: meal2,
+  kombi: kombi,
+}
+var thursday = {
+  name: 'Thursday',
+  meal1: meal1,
+  meal2: meal2,
+  kombi: kombi,
+}
+var friday = {
+  name: 'Friday',
+  meal1: meal1,
+  meal2: meal2,
+  kombi: kombi,
+}
+
+var week = [
+    monday,
+    thuesday,
+    wednesday,
+    thursday,
+    friday,
+]
+
+</script>
---
+++ src/Resources/vue/view/Costs.vue
@@ -0,0 +1,11 @@
+<template>
+Costs
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>

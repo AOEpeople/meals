751d075cfe0e615989b39674d5e196f201574ec1
Simon Henz <simon.henz@aoe.com>
2022-06-09 09:38:00 +0200

    added components

--- src/Resources/vue/components/Footer.vue
+++ src/Resources/vue/components/Footer.vue
@@ -2,10 +2,10 @@
   <footer class="bg-[rgb(225,232,238)]">
     <div class="grid mx-auto max-w-screen-aoe xl:grid-cols-2">
       <div class="items-center py-4">
-        <img class="mx-auto h-10 xl:mx-0" src="../../../../public/images/aoe-logo.svg" alt="AOE Logo" />
+        <img class="mx-auto h-10 xl:mx-0" src="../../images/aoe-logo.svg" alt="AOE Logo" />
       </div>
       <div class="hidden self-center text-right xl:inline-block">
-        <img class="inline-block mr-1 w-5" src="../../../../public/images/flag.svg" alt="flag" />
+        <img class="inline-block mr-1 w-5" src="../../images/flag.svg" alt="flag" />
         <a class="self-center cursor-pointer hover:text-highlight">
           {{ lang }} version
         </a>
@@ -14,6 +14,7 @@
     <div class="bg-[rgb(244,247,249)] w-full py-4 px-4 text-center">
       <a> @ 2022 AOE. All rights reserved </a>
     </div>
+    <router-link to="/guest">guest</router-link>
   </footer>
 </template>
 

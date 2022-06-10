2267c0e0842d22513752564a4d7760349bcab0c5
Simon Henz <simon.henz@aoe.com>
2022-06-10 15:24:30 +0200

    styled MainPage

--- src/Resources/vue/hooks/api.ts
+++ src/Resources/vue/hooks/api.ts
@@ -4,16 +4,16 @@ import { ref } from "vue";
 const instance = axios.create({
     baseURL: 'https://meals.test/',
     timeout: 1000,
-    headers: {'content-type': 'application/json'}
 });
 
-export default function useApi<T>(method: string, url: string, data?: JSON){
+export default function useApi<T>(method: string, url: string, contentType: string = 'application/json', data?: JSON){
     const response = ref<T>();
     const request = async () => {
         await instance({
             method: method,
             url: url,
             data: data,
+            headers: {'content-type': contentType },
         })
             .then((res) => response.value = res.data)
             .catch((error) => console.log(error));

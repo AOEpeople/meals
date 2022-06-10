0ecbd19c7eb4db3025b80c4d5e554f8450116dfd
Simon Henz <simon.henz@aoe.com>
2022-06-10 15:24:30 +0200

    styled MainPage

--- src/Resources/vue/components/navbar/MobileDropdown.vue
+++ src/Resources/vue/components/navbar/MobileDropdown.vue
@@ -1,11 +1,11 @@
 <template>
   <transition
       enter-active-class="transition duration-100 ease-out"
-      enter-from-class="transform scale-95 opacity-0"
-      enter-to-class="transform scale-100 opacity-100"
+      enter-from-class="transform scale-95"
+      enter-to-class="transform scale-100"
       leave-active-class="transition duration-75 ease-out"
-      leave-from-class="transform scale-100 opacity-100"
-      leave-to-class="transform scale-95 opacity-0"
+      leave-from-class="transform scale-100"
+      leave-to-class="transform scale-95"
   >
     <DisclosurePanel class="xl:hidden">
       <div class="pt-2 pb-3 space-y-1">
@@ -26,16 +26,16 @@
       <div class="pt-4 pb-3 border-t border-gray-200">
         <div class="flex items-center px-4">
           <div class="flex-shrink-0">
-            <img class="w-10 h-10" src="../../../images/person.svg" alt="Person" />
+            <Icons icon="person-outline" box="0 0 10 10" class="w-[10px] h-[10px] fill-primary" />
           </div>
           <div class="ml-3">
-            <div class="text-base font-medium text-gray-800">firstname.surname</div>
+            <div class="text-base font-medium text-gray-800">{{ props.userName }}</div>
           </div>
         </div>
         <div class="mt-3 space-y-1">
           <DisclosureButton as="router-link" to="/balance" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-highlight hover:bg-gray-100">Balance:
             <a class="text-primary hover:text-primary">
-              € {{ props.user.balance }}
+              € {{ props.balance }}
             </a>
           </DisclosureButton>
           <DisclosureButton as="a" href="/logout" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-highlight hover:bg-gray-100">Sign out</DisclosureButton>
@@ -46,9 +46,11 @@
 </template>
 
 <script setup>
-import { DisclosureButton, DisclosurePanel} from "@headlessui/vue";
+import { DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import Icons from '@/components/Icons.vue'
 const props = defineProps([
-    'user',
+    'userName',
+    'balance',
     'navigation',
 ]);
 

243d97fb706867592902e8e0638ede233b3d6d75
Simon Henz <simon.henz@aoe.com>
2022-06-21 15:02:10 +0200

    changed styling, added Tabs, renamed MealsMain to Dashboard, changed sorting of balance Table, fixed a few bugs

---
+++ src/Resources/src/components/dashboard/Week.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center">
+    <h2 class="m-0">{{ t(week.label) }}</h2>
+    <p class="description text-primary">{{t('Mon')}} 04.10. - {{ t('Fri') }} 08.10.</p>
+  </div>
+  <div class="grid" id="weekly-menu">
+    <Day v-for="day in props.week.value"
+         :day="day"
+         :key="day.name"
+         class="mb-[2.5rem]"
+    />
+  </div>
+</template>
+
+<script setup>
+import Day from '@/components/dashboard/Day.vue'
+import { useI18n } from "vue-i18n";
+const props = defineProps(['week']);
+const { t } = useI18n();
+
+</script>

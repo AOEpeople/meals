95f3f002b517938389f43531b57459e9f90a97db
Martin Tepper <martin.tepper@aoe.com>
2022-06-28 09:57:28 +0200

    253542: add progress bar for page load

--- src/Resources/src/App.vue
+++ src/Resources/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <NavBar />
+  <vue3-progress-bar></vue3-progress-bar>
   <Content />
   <Footer />
 </template>
 
 <script setup>
-import NavBar from "@/components/NavBar.vue"
-import Footer from "@/components/Footer.vue"
-import Content from "@/components/Content.vue";
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+import Content from '@/components/Content.vue'
 
-import { balanceStore } from "@/store/balanceStore";
+import { balanceStore } from '@/store/balanceStore'
 
-balanceStore.updateAmount(parseFloat(sessionStorage.getItem('balance')));
+balanceStore.updateAmount(parseFloat(sessionStorage.getItem('balance')))
 </script>

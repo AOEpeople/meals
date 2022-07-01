f8833da75e18f4255c6c8d67d15e4bbd49b949b0
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2022-07-01 11:49:21 +0200

    #253759 Update slot repository
    Implement and use SlotRepositoryInterface.

---
+++ src/Mealz/MealBundle/Repository/SlotRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mealz\MealBundle\Repository;
+
+class SlotRepository extends BaseRepository implements SlotRepositoryInterface
+{
+}
--- src/Mealz/MealBundle/Service/ParticipationServiceTrait.php
+++ src/Mealz/MealBundle/Service/ParticipationServiceTrait.php
@@ -7,15 +7,15 @@ namespace App\Mealz\MealBundle\Service;
 use App\Mealz\MealBundle\Entity\Meal;
 use App\Mealz\MealBundle\Entity\Participant;
 use App\Mealz\MealBundle\Entity\Slot;
-use App\Mealz\MealBundle\Entity\SlotRepository;
 use App\Mealz\MealBundle\Repository\ParticipantRepositoryInterface;
+use App\Mealz\MealBundle\Repository\SlotRepository;
 use App\Mealz\MealBundle\Service\Exception\ParticipationException;
 use App\Mealz\UserBundle\Entity\Profile;
 use DateTime;
 
 /**
  * @property ParticipantRepositoryInterface $participantRepo
- * @property SlotRepository        $slotRepo
+ * @property SlotRepository                 $slotRepo
  */
 trait ParticipationServiceTrait
 {

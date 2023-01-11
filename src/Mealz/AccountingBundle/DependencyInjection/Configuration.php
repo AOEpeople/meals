47724d584d84c2b69b77d640a01b91082632ccab
Simon Henz <simon.henz@aoe.com>
2023-01-11 09:26:05 +0100

    reverted some deletes

---
+++ src/Mealz/AccountingBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mealz\AccountingBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('mealz_accounting');
+        $rootNode = $treeBuilder->getRootNode();
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}

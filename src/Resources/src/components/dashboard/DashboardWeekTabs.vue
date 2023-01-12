06df49060ec3b8d6cca92f2e6e2f9bd1a11f76c0
Simon Henz <simon.henz@aoe.com>
2023-01-12 16:25:53 +0100

    styling and guestpage fixes

--- src/Resources/src/components/dashboard/DashboardWeekTabs.vue
+++ src/Resources/src/components/dashboard/DashboardWeekTabs.vue
@@ -41,3 +41,26 @@ const { t } = useI18n()
 
 const selectedTab = ref(Object.keys(props.weeks)[0])
 </script>
+
+<style scoped>
+.tabs {
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 300;
+  color: #173D7A;
+}
+.active-tab {
+  border-bottom-color: #173D7A;
+  border-bottom-width: 3px;
+  margin-bottom: 0;
+}
+.active {
+  border-bottom-color: #173D7A;
+  border-bottom-width: 3px;
+  margin-bottom: 0;
+}
+.tab {
+  padding-right: 0;
+  padding-left: 0;
+}
+</style>
--- src/Resources/src/components/dashboard/Slots.vue
+++ src/Resources/src/components/dashboard/Slots.vue
@@ -3,16 +3,16 @@
     v-model="selectedSlot"
     :disabled="disabled"
   >
-    <div class="relative mt-1">
-      <ListboxButton class="focus-visible:ring-offset-orange-300 relative w-full cursor-default bg-white py-2 pr-10 pl-3 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 sm:text-sm">
-        <span class="block truncate">
+    <div class="relative">
+      <ListboxButton class="flex items-center rounded-3xl border h-8 focus-visible:ring-offset-orange-300 relative w-64 cursor-default bg-white pr-10 pl-3 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2">
+        <span class="block truncate text-note text-gray">
           {{ selectedSlot.slug === 'auto' ? t('dashboard.slot.auto') : selectedSlot.title }}
           <span v-if="selectedSlot.limit !== 0">
             {{ '( ' + selectedSlot.count + ' / ' + selectedSlot.limit + ' )' }}
           </span>
         </span>
         <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          <SelectorIcon
+          <ChevronDownIcon
             class="h-5 w-5 text-gray-400"
             aria-hidden="true"
           />
@@ -24,7 +24,7 @@
         leave-from-class="opacity-100"
         leave-to-class="opacity-0"
       >
-        <ListboxOptions class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+        <ListboxOptions class="absolute max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-note shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
           <template v-for="slot in day.slots">
             <ListboxOption
               v-if="slot.id !== 0 || !isParticipating"
@@ -39,7 +39,7 @@
                   'relative cursor-default select-none py-2 pl-10 pr-4',
                 ]"
               >
-                <span :class="[selected ? 'font-medium' : 'font-normal', 'block truncate']">
+                <span :class="[selected ? 'font-medium' : 'font-normal', 'block truncate text-note']">
                   {{ slot.slug === 'auto' ? t('dashboard.slot.auto') : slot.title }}
                   <span v-if="slot.limit !== 0">
                     {{ '( ' + slot.count + ' / ' + slot.limit + ' )' }}
@@ -47,7 +47,7 @@
                 </span>
                 <span
                   v-if="selected"
-                  class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                  class="absolute inset-y-0 left-0 flex items-center pl-1 text-amber-600"
                 >
                   <CheckIcon
                     class="h-5 w-5"
@@ -74,56 +74,60 @@ import {
 
 import {useI18n} from "vue-i18n";
 import {useUpdateSelectedSlot} from "@/api/postUpdateSelectedSlot";
-import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid'
+import { CheckIcon, ChevronDownIcon } from '@heroicons/vue/solid'
 import {dashboardStore} from "@/stores/dashboardStore";
 
 const props = defineProps([
   'weekID',
   'dayID',
+    'day'
 ])
 const { t } = useI18n()
-const day = dashboardStore.getDay(props.weekID, props.dayID)
+
+const day = props.day ? props.day : dashboardStore.getDay(props.weekID, props.dayID)
+
 const meals = day.meals
 const selectedSlot = ref(day.slots[day.activeSlot])
 const activeSlot = computed(() => day.slots[day.activeSlot])
 const disabled = computed(() => day.isLocked)
-const isParticipating = ref(checkIfParticipating())
 
-watch(meals, () => {
-  isParticipating.value = checkIfParticipating()
-}, { deep: true })
+let isParticipating = ref(false)
 
-watch(activeSlot, () => {
-  selectedSlot.value = activeSlot.value
-})
+if (!props.day) {
+  isParticipating = ref(checkIfParticipating())
+  watch(meals, () => {
+    isParticipating.value = checkIfParticipating()
+  }, { deep: true })
+  watch(activeSlot, () => {
+    selectedSlot.value = activeSlot.value
+  })
 
-watch(selectedSlot, () => {
-  day.activeSlot = selectedSlot.value.id
-  console.log(isParticipating.value)
-  if (isParticipating.value) {
-    let data = {
-      slotID: selectedSlot.value.id,
-      dayID: props.dayID
+  watch(selectedSlot, () => {
+    day.activeSlot = selectedSlot.value.id
+    if (isParticipating.value) {
+      let data = {
+        slotID: selectedSlot.value.id,
+        dayID: props.dayID
+      }
+      useUpdateSelectedSlot(JSON.stringify(data))
     }
-    useUpdateSelectedSlot(JSON.stringify(data))
-  }
-})
+  })
 
-function checkIfParticipating() {
-  for (const mealId in meals) {
-    if (meals[mealId].variations !== null) {
-      for (const variationsId in meals[mealId].variations) {
-        if(meals[mealId].variations[variationsId].isParticipating) {
-          return true
+  function checkIfParticipating() {
+    for (const mealId in meals) {
+      if (meals[mealId].variations !== null) {
+        for (const variationsId in meals[mealId].variations) {
+          if(meals[mealId].variations[variationsId].isParticipating) {
+            return true
+          }
         }
       }
+      if(meals[mealId].isParticipating) {
+        return true
+      }
     }
-    if(meals[mealId].isParticipating) {
-      return true
-    }
+
+    return false
   }
-
-  return false
 }
-
 </script>

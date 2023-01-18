3bf43dee20fad8e46b9a083ea7e63532bbe15056
Simon Henz <simon.henz@aoe.com>
2023-01-18 13:21:24 +0100

    tabs styling and offer Popover (wip)

--- src/Resources/src/components/dashboard/DashboardWeekTabs.vue
+++ src/Resources/src/components/dashboard/DashboardWeekTabs.vue
@@ -1,7 +1,7 @@
 <template>
   <tabs
     v-model="selectedTab"
-    class="mb-5 justify-center"
+    class="tabs mb-5 justify-center gap-4"
   >
     <tab
       v-for="(week, weekID, index) in weeks"
@@ -9,8 +9,16 @@
       :val="weekID"
       :label="t('dashboard.' + index)"
       :indicator="true"
-      class="cursor-pointer"
-    />
+      :class="{'!border-b-[3px] !border-primary': weekID === selectedTab}"
+      class="cursor-pointer pb-2"
+    >
+      <div
+        :class="[weekID === selectedTab ? 'text-primary' : 'text-[#36404A]']"
+        class="tab text-note"
+      >
+        {{ t('dashboard.' + index) }}
+      </div>
+    </tab>
   </tabs>
   <tab-panels
     v-model="selectedTab"
@@ -41,26 +49,3 @@ const { t } = useI18n()
 
 const selectedTab = ref(Object.keys(props.weeks)[0])
 </script>
-
-<style scoped>
-.tabs {
-  font-size: 14px;
-  line-height: 20px;
-  font-weight: 300;
-  color: #173D7A;
-}
-.active-tab {
-  border-bottom-color: #173D7A;
-  border-bottom-width: 3px;
-  margin-bottom: 0;
-}
-.active {
-  border-bottom-color: #173D7A;
-  border-bottom-width: 3px;
-  margin-bottom: 0;
-}
-.tab {
-  padding-right: 0;
-  padding-left: 0;
-}
-</style>
---
+++ src/Resources/src/components/dashboard/OfferPopover.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="relative flex">
+    <div class="absolute bottom-[46px] z-10 h-auto w-auto rounded-[5px] bg-black/90 p-1 text-center">
+      <span class="whitespace-pre text-[11px] leading-[14px] text-white">{{ t('dashboard.popover') }}</span>
+    </div>
+    <div class="absolute left-[40px] h-0 w-0 border-x-[10px] border-t-[10px] border-black/90 border-x-transparent" />
+  </div>
+</template>
+<script setup>
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n()
+</script>

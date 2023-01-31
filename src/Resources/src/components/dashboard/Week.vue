c109fd228645452dff578c3ed6eea89fbc560154
Simon Henz <simon.henz@aoe.com>
2023-01-31 13:00:17 +0100

    working on environmentals (wip), fixed some bugs

--- src/Resources/src/components/dashboard/Week.vue
+++ src/Resources/src/components/dashboard/Week.vue
@@ -4,7 +4,7 @@
       {{ t('dashboard.' + index) }}
     </h2>
     <p class="description text-primary">
-      {{ (start + ' - ' + end).replaceAll(',', '') }}
+      {{ (startLocale + ' - ' + endLocale).replaceAll(',', '') }}
     </p>
   </div>
   <div
@@ -27,6 +27,7 @@ import Day from '@/components/dashboard/Day.vue'
 import { useI18n } from "vue-i18n"
 import { useProgress } from '@marcoschulte/vue3-progress'
 import {dashboardStore} from "@/stores/dashboardStore"
+import {computed} from "vue";
 
 const progress = useProgress().start()
 const { t, locale } = useI18n()
@@ -35,11 +36,11 @@ const props = defineProps(['weekID', 'index'])
 const week = dashboardStore.getWeek(props.weekID)
 const days = dashboardStore.getDays(props.weekID)
 
-let start = new Date(week.startDate.date)
-start = start.toLocaleDateString(locale.value, { weekday: 'short', month: 'numeric', day: 'numeric' })
+const startDate = new Date(week.startDate.date)
+const startLocale = computed(() => startDate.toLocaleDateString(locale.value, { weekday: 'short', month: 'numeric', day: 'numeric' }))
 
-let end = new Date(week.endDate.date)
-end = end.toLocaleDateString(locale.value, { weekday: 'short', month: 'numeric', day: 'numeric' })
+const endDate = new Date(week.endDate.date)
+const endLocale = computed(() => endDate.toLocaleDateString(locale.value, { weekday: 'short', month: 'numeric', day: 'numeric' }))
 
 setTimeout(function () {
   progress.finish()
---
+++ src/Resources/src/tools/getEnv.ts
@@ -0,0 +1,17 @@
+import {Env, useEnvs} from "@/api/getEnvironmentals";
+
+export default async function getEnv(): Promise<Env | undefined> {
+    const rawEnvs = sessionStorage.getItem("ENV")
+    if (rawEnvs !== null) {
+        return JSON.parse(rawEnvs) as Env
+    } else {
+
+        const { env, error } = await useEnvs();
+        if (error.value) {
+            console.log("couldn't receive Environmentals")
+            return undefined
+        } else {
+            return env
+        }
+    }
+}

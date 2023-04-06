d1ce72e8e9ad43bc5fb8647389beac1d8bad9d9f
Felix Ruf <felix.ruf@aoe.com>
2023-04-06 15:09:56 +0200

    meals of the current day are now displayed

--- src/Resources/src/components/participations/Meal.vue
+++ src/Resources/src/components/participations/Meal.vue
@@ -1,21 +1,41 @@
 <template>
-  <table>
+  <table 
+    v-if="meal" 
+    class="table-fixed border-2 border-solid border-black"
+  >
     <thead>
       <tr>
-        <th>{{ 'mealName' }}</th>
+        <th>{{ languageIsEnglish ? meal.title.en : meal.title.de }}</th>
       </tr>
     </thead>
-    <!-- <tbody v-if="variationOne && variationTwo">
+    <tbody v-if="meal.variations.length > 0">
       <tr>
-        <td>{{ variationOne }}</td>
-        <td>{{ variationTwo }}</td>
+        <td
+          v-for="(variation, index) in meal.variations"
+          :key="index"
+        >
+          {{ getTitleForLocale(variation) }}
+        </td>
       </tr>
-    </tbody> -->
+    </tbody>
   </table>
 </template>
 
 
 <script setup>
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
 
+const { locale } = useI18n();
+
+defineProps({
+  meal: null
+});
+
+const languageIsEnglish = computed(() => locale.value === 'en');
+
+function getTitleForLocale(variation) {
+  return languageIsEnglish.value ? variation.title.en : variation.title.de;
+}
 
 </script>

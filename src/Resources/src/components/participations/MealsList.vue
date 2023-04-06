cd0f6e666edcd6200eee1d0b537219a845cf6ae4
Felix Ruf <felix.ruf@aoe.com>
2023-04-06 14:09:53 +0200

    added composable and components to show the participations list

---
+++ src/Resources/src/components/participations/MealsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-row gap-1">
+    <Meal
+      v-for="(meal, index) in mealsWithVariations"
+      :key="index"
+      :meal="meal"
+    />
+  </div>
+</template>
+
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import Meal from './Meal.vue';
+import { getShowParticipations } from '@/api/getShowParticipations';
+// import type { IMealWithVariations } from '@/api/getShowParticipations';
+
+const { participationsState, loadShowParticipations, loadedState, getMealsWithVariations } = getShowParticipations();
+
+const mealsWithVariations = ref([]);
+
+onMounted(async () => {
+  await loadShowParticipations();
+  if(loadedState.loaded && loadedState.error === "") {
+    mealsWithVariations.value = getMealsWithVariations();
+  }
+});
+
+</script>
---
+++ src/Resources/src/views/ParticipantsList.vue
@@ -0,0 +1,14 @@
+<template>
+  <MealsList />
+</template>
+
+
+<script setup>
+import { usePrintableListData } from '@/api/getPrintableListData';
+import { useProgress } from '@marcoschulte/vue3-progress';
+import { useI18n } from 'vue-i18n';
+import MealsList from '@/components/participations/MealsList.vue';
+
+
+const { listData, error } = await usePrintableListData();
+</script>

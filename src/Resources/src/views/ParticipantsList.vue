d2420e23c27bd5cd2545021afce03a0f5c036267
Felix Ruf <felix.ruf@aoe.com>
2023-04-06 16:51:28 +0200

    added typescript support for .vue sfc's via the lang="ts" option

--- src/Resources/src/views/ParticipantsList.vue
+++ src/Resources/src/views/ParticipantsList.vue
@@ -3,12 +3,12 @@
 </template>
 
 
-<script setup>
+<script setup lang="ts">
 import { usePrintableListData } from '@/api/getPrintableListData';
 import { useProgress } from '@marcoschulte/vue3-progress';
 import { useI18n } from 'vue-i18n';
 import MealsList from '@/components/participations/MealsList.vue';
 
 
-const { listData, error } = await usePrintableListData();
+
 </script>

416f8d684e60aef55f6060e053bc1619942fcb9d
Felix Ruf <felix.ruf@aoe.com>
2023-04-11 14:00:25 +0200

    added a table, containing the participants and the meals they ordered

---
+++ src/Resources/src/components/participations/ParticipantsTableHead.vue
@@ -0,0 +1,29 @@
+<template>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Kombi</th>
+      <th
+        v-for="meal in mealsList"
+        :key="meal"
+      >
+        {{ meal }}
+      </th>
+    </tr>
+  </thead>
+</template>
+
+<script setup lang="ts">
+import { getShowParticipations } from '@/api/getShowParticipations';
+import { onMounted, ref } from 'vue';
+
+const { loadShowParticipations, getListOfMeals } = getShowParticipations();
+
+const mealsList = ref<string[]>([]);
+
+onMounted(async () => {
+  await loadShowParticipations();
+  mealsList.value = getListOfMeals();
+});
+
+</script>

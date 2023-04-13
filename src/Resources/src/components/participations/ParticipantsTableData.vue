7dcc24226f9e0c05712a1d5f516332faf856a169
Felix Ruf <felix.ruf@aoe.com>
2023-04-13 17:16:30 +0200

    added basic styling to the table and cleaned up some unused code

---
+++ src/Resources/src/components/participations/ParticipantsTableData.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    v-if="meal.variations.length > 0"
+    class="flex h-full w-full flex-row"
+  >
+    <div
+      v-for="(variation, index) in meal.variations"
+      :key="variation.mealId"
+      class="h-full w-full flex-1 text-center"
+    >
+      <svg
+        v-if="bookedMeals.booked.includes(variation.mealId)"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="text-primary m-auto block h-6 w-6"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </div>
+  </div>
+  <div
+    v-else
+    class="h-full w-full text-center"
+  >
+    <svg
+      v-if="bookedMeals.booked.includes(meal.mealId)"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="text-primary m-auto block h-6 w-6"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IBookedData, IMealWithVariations } from '@/api/getShowParticipations';
+
+defineProps<{
+  bookedMeals: IBookedData,
+  meal: IMealWithVariations
+}>();
+</script>

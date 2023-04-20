6597f2c2f4101ebcf8cd15b39d79779821af330f
Felix Ruf <felix.ruf@aoe.com>
2023-04-20 16:32:17 +0200

    auto-scrolling the table now works

--- src/Resources/src/components/participations/ParticipantsTableBody.vue
+++ src/Resources/src/components/participations/ParticipantsTableBody.vue
@@ -1,5 +1,8 @@
 <template>
-  <tbody class="grow-1 shrink-1 block basis-auto overflow-y-auto overflow-x-hidden">
+  <tbody
+    ref="tableBody"
+    class="grow-1 shrink-1 block basis-auto overflow-y-auto overflow-x-hidden"
+  >
     <ParticipantsTableSlot
       v-for="(participants, slot) in participationsState.data"
       :key="slot"
@@ -14,9 +17,18 @@
 
 import { getShowParticipations } from '@/api/getShowParticipations';
 import ParticipantsTableSlot from './ParticipantsTableSlot.vue';
-import { computed } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
 
 const { participationsState, getMealsWithVariations, loadedState } = getShowParticipations();
+
+const tableBody = ref<HTMLTableSectionElement | null>(null);
+const scrollDirectionDown = ref(true);
+const INTERVAL_DELAY = 16;
+const SCROLL_AMOUNT = 1;
+const TIMEOUT_AFTER_SCROLLING = 3000;
+
+let scrollProcessId:number;
+let scrollingActive = true;
 
 const mealsWithVariations = computed(() => {
   if(loadedState.loaded && loadedState.error === "") {
@@ -25,4 +37,47 @@ const mealsWithVariations = computed(() => {
     return [];
   }
 });
+
+onMounted(() => {
+  scrollProcessId = window.setInterval(() => scrollDown(tableBody.value), INTERVAL_DELAY);
+});
+
+onUnmounted(() => {
+  if(scrollProcessId) {
+    clearInterval(scrollProcessId);
+  }
+})
+
+function scrollDown(element: HTMLTableSectionElement | null) {
+  if(scrollingActive && element) {
+    setScrollDirection(element)
+    element.scrollBy({
+      top: scrollDirectionDown.value ? SCROLL_AMOUNT : -SCROLL_AMOUNT,
+      behavior: 'smooth'
+    });
+  }
+}
+
+function setScrollDirection(element: HTMLTableSectionElement | null) {
+  const cachedScrollDirection = scrollDirectionDown.value;
+  if(element && scrollDirectionDown.value) {
+    scrollDirectionDown.value = (element.scrollTop + element.clientHeight) < element.scrollHeight;
+  } else if(element && !scrollDirectionDown.value) {
+    scrollDirectionDown.value = element.scrollTop === 0;
+  }
+
+  if(cachedScrollDirection !== scrollDirectionDown.value) {
+    sleep(TIMEOUT_AFTER_SCROLLING);
+  }
+
+}
+
+function sleep(ms: number) {
+  scrollingActive = false;
+  setTimeout(() => scrollingActive = true, ms);
+}
 </script>
+
+<style scoped>
+
+</style>

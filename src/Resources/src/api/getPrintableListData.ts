0ebf23376492ed082e357e8d7100a65dd0cb80ee
Felix Ruf <felix.ruf@aoe.com>
2023-04-25 13:54:24 +0200

    fixed PrintableList to work with the new api response on '/api/print/participations'

--- src/Resources/src/api/getPrintableListData.ts
+++ src/Resources/src/api/getPrintableListData.ts
@@ -1,30 +1,60 @@
 import useApi from "@/api/api";
-import { ref } from "vue"
+import { onMounted, reactive, readonly, ref } from "vue"
 import type {Dictionary} from "../../types/types";
 import type {DateTime} from "@/api/getDashboardData"
 
-type ListData = {
-    data: Dictionary<Dictionary<boolean>>
-    meals: {
+export type ListData = {
+    data: Dictionary<Dictionary<Dictionary<Array<number>>>>
+    meals: Dictionary<MealData>,
+    day: DateTime
+}
+
+type MealData = {
+    title: {
         en: string,
         de: string
     },
-    day: DateTime,
-    participations: Dictionary<number>
+    parent?: number | null,
+    participations?: number
 }
 
-export async function usePrintableListData(){
-    const { response: listData, request, error } = useApi<ListData>(
-        "GET",
-        "/api/print/participations",
-    );
+const listDataState = reactive<ListData>({
+    data: {},
+    meals: {},
+    day: {
+        date: "",
+        timezone_type: 0,
+        timezone: ""
+    }
+});
+
+export function usePrintableListData(){
 
     const loaded = ref(false)
 
-    if (loaded.value === false) {
-        await request()
-        loaded.value = true
+    onMounted(async () => {
+        await getListData();
+    });
+
+    async function getListData() {
+        const { response: listData, request, error } = useApi<ListData>(
+            "GET",
+            "/api/print/participations",
+        );
+
+        if (loaded.value === false) {
+            await request();
+            loaded.value = true;
+
+            if(!error.value && listData.value) {
+                listDataState.data = listData.value.data;
+                listDataState.meals = listData.value.meals;
+                listDataState.day = listData.value.day;
+            }
+        }
     }
 
-    return { listData, error }
+    return {
+        listData: readonly(listDataState)
+    }
 }

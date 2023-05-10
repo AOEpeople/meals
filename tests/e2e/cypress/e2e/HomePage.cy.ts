6efd89f6446787db39d91e509024133fdd9492f1
Felix Ruf <felix.ruf@aoe.com>
2023-05-10 13:54:16 +0200

    added cypress and added a basic e2e test

---
+++ tests/e2e/cypress/e2e/HomePage.cy.ts
@@ -0,0 +1,5 @@
+describe('Test Base URL and login', () => {
+    it('should be able to visit the page and login', () => {
+      cy.visit('/');
+    });
+  });
---
+++ tests/e2e/cypress/support/index.ts
@@ -0,0 +1,5 @@
+import "cypress-plugin-snapshots/commands";
+import "cypress-keycloak";
+import "./commands";
+import "./events";
+import "./interceptors";

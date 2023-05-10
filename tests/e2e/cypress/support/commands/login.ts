66372bdf6fc77c65259c4b11781bf21eef67d0dc
Felix Ruf <felix.ruf@aoe.com>
2023-05-10 13:54:16 +0200

    added cypress and added a basic e2e test

---
+++ tests/e2e/cypress/support/commands/login.ts
@@ -0,0 +1,45 @@
+export const login = (username: string, password: string) => {
+  logout();
+  const loginFn = Cypress.env('oauth_enable') ? oauthLogin : simpleLogin;
+  loginFn(username, password);
+};
+
+export const loginAs = (user: string) => {
+  cy.fixture(`users/${user}.json`).then((credentials) => {
+    login(credentials.username, credentials.password);
+  });
+};
+
+export const logout = () => {
+  Cypress.env('oauth_enable') ? oauthLogout() : simpleLogout();
+};
+
+const oauthLogin = (username: string, password: string) => {
+  cy.login({
+    root: Cypress.env('oauth_base_url'),
+    realm: Cypress.env('oauth_realm'),
+    username: username,
+    password: password,
+    client_id: Cypress.env('oauth_client_id'),
+    redirect_uri: Cypress.env('baseUrl') + Cypress.env('oauth_redirect_uri'),
+  });
+}
+
+const oauthLogout = () => {
+  cy.request({
+    url: `${Cypress.env('oauth_base_url')}/auth/realms/${Cypress.env('oauth_realm')}/protocol/openid-connect/logout`,
+    followRedirect: false
+  });
+};
+
+const simpleLogin = (username: string, password: string) => {
+  cy.visit(Cypress.env('baseUrl') + 'login');
+  cy.get("#username").type(username);
+  cy.get("#password").type(password);
+  cy.get("#password").closest("form").submit()
+}
+
+const simpleLogout = () => {
+  cy.visit(Cypress.env('baseUrl') + 'logout');
+};
+

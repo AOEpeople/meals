0f659bc798de57cfa8613251287f4996f4ebb57f
Felix Ruf <felix.ruf@aoe.com>
2023-05-11 11:29:21 +0200

    added automatic login

--- tests/e2e/cypress/e2e/HomePage.cy.ts
+++ tests/e2e/cypress/e2e/HomePage.cy.ts
@@ -1,5 +1,14 @@
 describe('Test Base URL and login', () => {
-    it('should be able to visit the page and login', () => {
-      cy.visit('/');
-    });
+
+  beforeEach(() => {
+    cy.setCookie('locale', 'en');
+    cy.loginAs('kochomi');
+    cy.visitMeals();
   });
+
+  it('should be able to visit the page and login', () => {
+    cy.visit('/');
+    cy.contains(/Aktuelle Woche/);
+    cy.contains(/Nächste Woche/);
+  });
+});

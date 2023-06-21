9c5dc1885febba6db9bf32e988242d1c87652484
Felix Ruf <felix.ruf@aoe.com>
2023-06-21 17:42:31 +0200

    added a component to select and search dishes

---
+++ src/Resources/src/components/menu/MenuDay.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h4>Input Test</h4>
+    <MenuInput />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MenuInput from '@/components/menu/MenuInput.vue';
+
+</script>

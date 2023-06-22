263f0db54b972b09276aefad1a12d8a04ac80fe1
Felix Ruf <felix.ruf@aoe.com>
2023-06-22 13:16:29 +0200

    finished the MenuInput component

--- src/Resources/src/components/menu/MenuDay.vue
+++ src/Resources/src/components/menu/MenuDay.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h4>Input Test</h4>
+    <MenuInput
+      v-model="selectedDishOne"
+    />
     <MenuInput />
   </div>
 </template>
 
 <script setup lang="ts">
 import MenuInput from '@/components/menu/MenuInput.vue';
+import { Dish } from '@/stores/dishesStore';
+import { ref } from 'vue';
 
+const selectedDishOne = ref<Dish | null>(null);
+const selectedDishTwo = ref<Dish | null>(null);
 </script>

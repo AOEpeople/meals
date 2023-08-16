04c4c1dc9fc5b7ed70f4e05d627495bd6d247a6e
Felix Ruf <felix.ruf@aoe.com>
2023-08-16 17:34:15 +0200

    added mailhog support to cypress

---
+++ tests/e2e/cypress/support/commands/mailhog.ts
@@ -0,0 +1,10 @@
+export const visitSettlementLinkFromMail = () => {
+    cy.request(`${Cypress.env('mailhog_url')}/api/v2/messages?start=0&limit=1`).then((response) => {
+        const html: string = response.body.items[0].MIME.Parts[0].Body;
+        cy.log(`Body: ${html}`);
+        const replaced = html.replace(/(\r\n|\n|\r|=)/gm, "");
+        cy.log(`Replaced: ${replaced}`);
+        const confirmationURL = replaced.split('https://meals.test/')[1];
+        cy.visit(`${Cypress.env('baseUrl')}${confirmationURL}`);
+    });
+}

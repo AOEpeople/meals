9cdfeca5db88b08dd20dfb032535a44282875907
Martin Tepper <martin.tepper@aoe.com>
2023-10-11 15:44:12 +0200

    Fix linter messages

--- src/Mealz/MealBundle/Form/MealAdmin/EventForm.php
+++ src/Mealz/MealBundle/Form/MealAdmin/EventForm.php
@@ -8,7 +8,6 @@ use App\Mealz\MealBundle\Entity\Event;
 use App\Mealz\MealBundle\Form\Type\EntityHiddenType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -36,46 +35,17 @@ class EventForm extends AbstractType
             ]);
 
         $builder->addEventListener(FormEvents::PRE_SET_DATA, static function (FormEvent $formEvent) {
-            /** @var Meal|null $meal */
+            /** @var Event|null $event */
             $event = $formEvent->getData();
             if (null === $event) {
                 return;
             }
-            $day = $meal->getDay();
-            $week = $day->getWeek();
-            if (false === $day->isEnabled() || false === $week->isEnabled()) {
-                $form = $event->getForm();
-                $config = $form->get('event')->getConfig();
-                $opts = $config->getOptions();
-                $opts['attr'] = ['readonly' => 'readonly'];
-                $form->add('event', EntityHiddenType::class, $opts);
-            }
-        });
 
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
-            /** @var array $data client submitted event data */
-            $data = $event->getData();
-
-            if (isset($data['dish']) && '' === $data['dish']) {
-                // Empty dish value from client means meal needs to be deleted.
-                // We cannot set meal's dish to null, so we just mark the meal for removal.
-                /** @var Meal $meal */
-                $meal = $event->getForm()->getData();
-                $data['dish'] = $meal->getDish()->getId();  // restore original dish value
-                $this->entityManager->remove($meal);        // mark the meal to be removed
-                $meal->getDay()->removeMeal($meal);
-
-                $event->setData($data);
-                $event->getForm()->setData($meal);
-            }
-        });
-
-        $builder->addEventListener(FormEvents::SUBMIT, static function (FormEvent $event) {
-            /** @var Event $event */
-            $event = $event->getData();
-            $dishPrice = $meal->getDish()->getPrice();
-            $meal->setPrice($dishPrice);
-            $event->setData($meal);
+            $form = $formEvent->getForm();
+            $config = $form->get('event')->getConfig();
+            $opts = $config->getOptions();
+            $opts['attr'] = ['readonly' => 'readonly'];
+            $form->add('event', EntityHiddenType::class, $opts);
         });
     }
 
@@ -88,9 +58,8 @@ class EventForm extends AbstractType
             'data_class' => Event::class,
             'empty_data' => static function (FormInterface $form) {
                 $event = $form->get('event')->getData();
-                $day = $form->getParent()->getParent()->getData();
+
                 return $event;
-                #return new Event($event, $day);
             },
             'error_bubbling' => false,
         ]);

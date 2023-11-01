743c015e9739cc0016d593453844cc62ffea14ba
Felix Ruf <felix.ruf@aoe.com>
2023-11-01 11:19:49 +0100

    fixed tests

--- tests/e2e/cypress/support/commands/mailhog.ts
+++ tests/e2e/cypress/support/commands/mailhog.ts
@@ -1,9 +1,14 @@
 export const visitSettlementLinkFromMail = () => {
     cy.request(`${Cypress.env('mailhog_url')}/api/v2/messages?start=0&limit=1`).then((response) => {
-        cy.log(response.body);
-        const html: string = response.body.items[0].MIME.Parts[0].Body;
+        cy.log('Checking response of mailhog...');
+        expect(response.status).to.eq(200);
+        const html = response.body;
         cy.log(`Body: ${html}`);
-        const replaced = html.replace(/(\r\n|\n|\r|=)/gm, "");
+        const items = html.items[0];
+        cy.log(`items: ${items}`);
+        const mime = items.MIME;
+        const body = mime.Parts[0].Body;
+        const replaced = body.replace(/(\r\n|\n|\r|=)/gm, "");
         cy.log(`Replaced: ${replaced}`);
         const confirmationURL = replaced.split(Cypress.env('baseUrl'))[1];
         cy.visit(`${Cypress.env('baseUrl')}${confirmationURL}`);

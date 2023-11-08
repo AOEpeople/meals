28647b699aff1e1474eddac30715f14bdd3516b2
Felix Ruf <felix.ruf@aoe.com>
2023-11-08 10:49:58 +0100

    removed unnecessary import

--- src/Mealz/UserBundle/Controller/SecurityController.php
+++ src/Mealz/UserBundle/Controller/SecurityController.php
@@ -4,7 +4,6 @@ namespace App\Mealz\UserBundle\Controller;
 
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;

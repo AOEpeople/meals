d4fd031efc63fb485f78219237b05a116f84e41f
Martin Tepper <martin.tepper@aoe.com>
2023-11-08 12:13:30 +0100

    Fix migrations

---
+++ migrations/Version20231108110441.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231108110441 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, deleted TINYINT(1) DEFAULT 0 NOT NULL, slug VARCHAR(128) NOT NULL, public TINYINT(1) DEFAULT 0 NOT NULL, UNIQUE INDEX UNIQ_3BAE0AA7989D9B62 (slug), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event_participation (id INT AUTO_INCREMENT NOT NULL, day INT DEFAULT NULL, event INT DEFAULT NULL, UNIQUE INDEX UNIQ_8F0C52E3E5A02990 (day), INDEX IDX_8F0C52E33BAE0AA7 (event), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE day ADD event_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE participant ADD event INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE event_participation');
+        $this->addSql('ALTER TABLE day DROP event_id');
+        $this->addSql('ALTER TABLE participant DROP event');
+    }
+}

a66bf4e8c462d40d9c001cf3c6d001194871c0bc
Felix Ruf <felix.ruf@aoe.com>
2023-11-15 13:08:28 +0100

    removed unused code fragments

--- src/Mealz/MealBundle/Controller/FrontendController.php
+++ src/Mealz/MealBundle/Controller/FrontendController.php
@@ -10,12 +10,6 @@ class FrontendController extends BaseController
 {
     public function renderIndex(): Response
     {
-        // $baseUrl = $this->getParameter('app.base_url');
-        // if ('oauth' === $this->getParameter('app.auth.mode') && null === $this->getUser()) {
-        //     return $this->redirect($baseUrl . '/weeks');
-        // } else if ('json' === $this->getParameter('app.auth.mode') && null === $this->getUser()){
-        //     return $this->redirect($baseUrl . '/login');
-        // }
         return $this->render('MealzMealBundle:Meals:index.html.twig');
     }
 }

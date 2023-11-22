cd5693466beacf565b277eefae0df6a065b825b3
Felix Ruf <felix.ruf@aoe.com>
2023-11-22 16:24:48 +0100

    fixed a bug that made the date input behave different for different timezones, also updated the input to use the VueDatePicker

--- src/Resources/src/components/menu/MenuLockDatePicker.vue
+++ src/Resources/src/components/menu/MenuLockDatePicker.vue
@@ -23,13 +23,14 @@
           <span class="w-full self-center truncate text-left text-xs font-medium text-[#173D7A]">
             {{ t('menu.lockDate') }}
           </span>
-          <input
-            :value="getLockDateAsStrRepr(new Date(lockDate.date))"
-            type="datetime-local"
-            class="w-full rounded-full border-2 border-solid border-[#CAD6E1] px-4 py-2 text-center text-[14px] text-[#9CA3AF]"
-            data-cy="meal-lockdate-input"
-            @change="event => lockDate.date = convertDateReprToLockDayFormat((event.target as HTMLInputElement).value)"
-          >
+          <VueDatePicker
+            v-model="date"
+            :enable-time-picker="true"
+            :time-picker-inline="true"
+            :clearable="false"
+            :format="locale.includes('de') ? 'dd.MM.yyyy HH:mm' : 'MM/dd/yyyy HH:mm'"
+            auto-apply
+          />
         </label>
       </div>
     </template>
@@ -42,18 +43,31 @@ import { CalendarIcon } from '@heroicons/vue/solid';
 import { useI18n } from 'vue-i18n';
 import { DateTime } from '@/api/getDashboardData';
 import { XCircleIcon } from '@heroicons/vue/solid';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import { computed } from 'vue';
 
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
-defineProps<{
+const props = defineProps<{
   lockDate: DateTime
 }>();
 
+const date = computed({
+  get() {
+    return props.lockDate.date
+  },
+  set(value: Date | string) {
+    if(typeof(value) !== 'string') {
+      props.lockDate.date = convertDateReprToLockDayFormat(getLockDateAsStrRepr(value))
+    }
+  }
+})
+
 // output format: 2023-07-19T12:00
 function getLockDateAsStrRepr(date: Date) {
-  return new Date(date.setTime(date.getTime() + (2*60*60*1000)))
-    .toISOString()
-    .substring(0, 16);
+  const timezoneMillisOffset = date.getTimezoneOffset() * 60 * 1000;
+  date.setTime(date.getTime() - timezoneMillisOffset);
+  return date.toISOString().substring(0, 16);
 }
 
 // output format: 2023-07-13 16:00:00.000000

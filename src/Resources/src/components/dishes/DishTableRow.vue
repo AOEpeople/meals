4c1a469c5195df51f9a68e1391a4aa6d2b419a80
Felix Ruf <felix.ruf@aoe.com>
2023-11-23 13:29:55 +0100

    added a table row component, that renders lazy

--- src/Resources/src/components/dishes/DishTableRow.vue
+++ src/Resources/src/components/dishes/DishTableRow.vue
@@ -1,53 +1,59 @@
 <template>
-  <tr class="border-b-2 border-gray-200">
-    <td
-      colspan="1"
-      class="w-[50%] py-2"
-    >
-      <span class="text-[12px] xl:text-[18px]">
-        {{ locale === 'en' ? dish.titleEn : dish.titleDe }}
-      </span>
-    </td>
-    <td
-      colspan="1"
-      class="w-[10%] text-[12px] xl:text-[18px]"
-    >
-      {{ getCategoryTitleById(dish.categoryId, locale) }}
-    </td>
-    <td
-      colspan="1"
-      class="w-[40%]"
-    >
-      <DishActions
-        :dish="dish"
-        :index="indexInList"
-      />
-    </td>
-  </tr>
-  <tr
-    v-for="(variation, index) in dish.variations"
-    :key="variation.slug"
-    class="overflow-hidden border-b-2 border-gray-200"
-    :class="[index === 0 ? 'topShadow' : 'bottomShadow', dish.variations.length === 1 ? 'topBottomShadow' : '']"
+  <Lazy
+    :render-on-idle="true"
+    :min-height="40"
+    :unrender="true"
   >
-    <td
-      colspan="1"
-      class="w-[50%] py-2 pl-4"
+    <tr class="border-b-2 border-gray-200">
+      <td
+        colspan="1"
+        class="w-[50%] py-2"
+      >
+        <span class="text-[12px] xl:text-[18px]">
+          {{ locale === 'en' ? dish.titleEn : dish.titleDe }}
+        </span>
+      </td>
+      <td
+        colspan="1"
+        class="w-[10%] text-[12px] xl:text-[18px]"
+      >
+        {{ getCategoryTitleById(dish.categoryId, locale) }}
+      </td>
+      <td
+        colspan="1"
+        class="w-[40%]"
+      >
+        <DishActions
+          :dish="dish"
+          :index="indexInList"
+        />
+      </td>
+    </tr>
+    <tr
+      v-for="(variation, index) in dish.variations"
+      :key="variation.slug"
+      class="overflow-hidden border-b-2 border-gray-200"
+      :class="[index === 0 ? 'topShadow' : 'bottomShadow', dish.variations.length === 1 ? 'topBottomShadow' : '']"
     >
-      <span class="text-[12px] xl:text-[18px]">
-        {{ locale === 'en' ? variation.titleEn : variation.titleDe }}
-      </span>
-    </td>
-    <td
-      colspan="2"
-      class="w-[50%]"
-    >
-      <DishVariationActions
-        :variation="variation"
-        :parent-slug="dish.slug"
-      />
-    </td>
-  </tr>
+      <td
+        colspan="1"
+        class="w-[50%] py-2 pl-4"
+      >
+        <span class="text-[12px] xl:text-[18px]">
+          {{ locale === 'en' ? variation.titleEn : variation.titleDe }}
+        </span>
+      </td>
+      <td
+        colspan="2"
+        class="w-[50%]"
+      >
+        <DishVariationActions
+          :variation="variation"
+          :parent-slug="dish.slug"
+        />
+      </td>
+    </tr>
+  </Lazy>
 </template>
 
 <script setup lang="ts">
@@ -56,6 +62,7 @@ import { useI18n } from 'vue-i18n';
 import { useCategories } from '@/stores/categoriesStore';
 import DishActions from './DishActions.vue';
 import DishVariationActions from './DishVariationActions.vue';
+import Lazy from '../misc/Lazy.vue';
 
 const { locale } = useI18n();
 const { getCategoryTitleById } = useCategories();

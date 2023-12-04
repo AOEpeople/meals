4846f3023aeb2c89eebcd96a0676881ae2e6403d
AYAxDesign <aya.akutsu@aoe.com>
2023-12-04 14:59:55 +0100

    participants are listed

--- src/Resources/src/components/participations/filterParticipantsList.ts
+++ src/Resources/src/components/participations/filterParticipantsList.ts
@@ -1,21 +1,20 @@
 import { useParticipationsListData } from "@/api/getParticipationsByDay";
 import { computed } from "vue";
 
-interface ParticipantState {
-  participants: any,
-  filter: string,
-  isLoading: boolean,
-  error: string
-}
+// interface ParticipantState {
+//   participants: any,
+//   filter: string,
+//   isLoading: boolean,
+//   error: string
+// }
 
-export interface Participant {
-  name: string
-}
+// export interface Participant {
+//   name: string
+// }
 
 export function filterParticipantsList(date: string){
 
   const { listData } = useParticipationsListData(date);
-  //const fruits: string[] = ['Apple', 'Orange', 'Banana'];
 
 
   function setFilter(filterStr: string) {

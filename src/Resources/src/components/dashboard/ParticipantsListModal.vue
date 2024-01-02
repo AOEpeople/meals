7d9c8b26146f0cc170e228a84de66c5f974081f2
AYAxDesign <aya.akutsu@aoe.com>
2024-01-02 14:43:53 +0100

    Improve the layout and styling of the container by introducing spacing and adjusting the positions of child elements and addes XIcon

--- src/Resources/src/components/dashboard/ParticipantsListModal.vue
+++ src/Resources/src/components/dashboard/ParticipantsListModal.vue
@@ -8,26 +8,8 @@
       class="fixed inset-0 flex items-center justify-center bg-black/30 p-4"
     >
       <DialogPanel
-        class="relative inset-0 max-h-96 w-96 overflow-scroll rounded-lg bg-white p-2 text-left drop-shadow-2xl sm:my-8 sm:p-6"
+        class="day-shadow relative inset-0 mx-auto h-auto w-max overflow-scroll rounded-lg bg-white p-2 text-left drop-shadow-2xl sm:p-6"
       >
-        <DialogTitle
-          class="mr-2 inline-block text-[11px] font-bold uppercase leading-4 tracking-[1.5px] text-primary"
-        >
-          {{ t('dashboard.print') }}
-        </DialogTitle>
-        <p>Filter: {{ filterInput }}</p>
-        <!-- <input
-          v-model="filterString"
-          :placeholder="t('costs.search')"
-          class="col-span-3 row-start-2 justify-self-center sm:col-span-1 sm:col-start-1 sm:justify-self-start min-[900px]:row-start-2"
-          @input="$emit('update:filterValue')"
-        > -->
-        <InputLabel
-          v-model="filterInput"
-          :label-text="t('dish.search')"
-          :label-visible="false"
-          class="col-span-3 row-start-2 justify-self-center sm:col-span-1 sm:col-start-1 sm:justify-self-start min-[900px]:row-start-2"
-        />
         <ParticipantsListByDay
           :date="date"
         />
@@ -45,8 +27,7 @@
 </template>
 
 <script setup lang="ts">
-import InputLabel from '@/components/misc/InputLabel.vue';
-import { Dialog, DialogPanel, DialogTitle } from '@headlessui/vue';
+import { Dialog, DialogPanel } from '@headlessui/vue';
 import { useI18n } from 'vue-i18n';
 import CancelButton from '../misc/CancelButton.vue';
 import ParticipantsListByDay from '../participations/ParticipantsListByDay.vue';
---
+++ src/Resources/src/components/misc/IconCancel.vue
@@ -0,0 +1,22 @@
+<template>
+  <XIcon
+    icon="guest"
+    box="0 0 13 13"
+    class="h-[13px] w-[13px] flex-none fill-primary"
+    @mousedown="buttonPressed = true"
+    @mouseup="buttonPressed = false"
+  />
+</template>
+
+<script setup lang="ts">
+import { XIcon } from '@heroicons/vue/solid';
+import { ref } from 'vue';
+
+const buttonPressed = ref(false);
+</script>
+
+<style scoped>
+.light-shadow {
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0), 0px 0px 0px 0px rgba(0, 0, 0, 0) , 0px 3px 0 0 rgba(192, 210, 226, 1);
+}
+</style>

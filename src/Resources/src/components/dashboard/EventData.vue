3c748b9e34aafc1494497d64afa4b65c23e5140f
Felix Ruf <felix.ruf@aoe.com>
2024-01-17 16:18:25 +0100

    added indicators to events in the dashboard

--- src/Resources/src/components/dashboard/EventData.vue
+++ src/Resources/src/components/dashboard/EventData.vue
@@ -1,28 +1,43 @@
 <template>
   <div
-    class="flex w-full flex-row items-center px-[15px] py-[13px]"
+    class="grid w-full grid-cols-1 grid-rows-[minmax(0,2fr)_auto_minmax(0,1fr)]"
   >
-    <div class="grid w-full grid-cols-[24px_minmax(0,1fr)] gap-1">
+    <BannerSpacer>
       <EventIcon
-        class="w-[24px] self-center"
+        class="w-[20px] self-center"
+        :size="20"
       />
+      <span
+        class="text-[14px] font-bold leading-[20px] tracking-[0.5px] text-primary-1"
+      >
+        {{ t('event.event') }}
+      </span>
+      <EventIcon
+        class="w-[20px] self-center"
+        :size="20"
+      />
+    </BannerSpacer>
+    <div
+      class="flex w-full flex-row items-center px-[15px]"
+    >
       <span class="mr-[5px] inline-block grow self-center break-words text-[12px] font-bold leading-[20px] tracking-[0.5px] text-primary-1 min-[380px]:text-note">
         {{ getEventById(day.event.eventId)?.title }}
       </span>
+      <ParticipationCounter
+        class="justify-self-end"
+        :limit="0"
+        :mealCSS="new Date(day.date.date) > new Date() ? 'bg-primary-4' : 'bg-[#80909F]'"
+      >
+        {{ day.event?.participations }}
+      </ParticipationCounter>
+      <CheckBox
+        class="justify-self-end"
+        :isActive="new Date(day.date.date) > new Date()"
+        :isChecked="day.event?.isParticipating ?? false"
+        @click="day.event?.isParticipating === false ? joinEvent(day.date.date) : leaveEvent(day.date.date)"
+      />
     </div>
-    <ParticipationCounter
-      class="justify-self-end"
-      :limit="0"
-      :mealCSS="new Date(day.date.date) > new Date() ? 'bg-primary-4' : 'bg-[#80909F]'"
-    >
-      {{ day.event?.participations }}
-    </ParticipationCounter>
-    <CheckBox
-      class="justify-self-end"
-      :isActive="new Date(day.date.date) > new Date()"
-      :isChecked="day.event?.isParticipating ?? false"
-      @click="day.event?.isParticipating === false ? joinEvent(day.date.date) : leaveEvent(day.date.date)"
-    />
+    <div />
   </div>
 </template>
 
@@ -32,10 +47,13 @@ import { useEvents } from '@/stores/eventsStore';
 import ParticipationCounter from '../menuCard/ParticipationCounter.vue';
 import CheckBox from '../misc/CheckBox.vue';
 import EventIcon from '../misc/EventIcon.vue';
+import BannerSpacer from '../misc/BannerSpacer.vue';
+import { useI18n } from 'vue-i18n';
 
 defineProps<{
   day: Day
 }>();
 
+const { t } = useI18n();
 const { getEventById, joinEvent, leaveEvent } = useEvents();
 </script>

b015daa5cb02dedd9344cb91aa39af06793e8d32
Felix Ruf <felix.ruf@aoe.com>
2024-01-31 14:19:58 +0100

    added tests for new event invitation functionality

---
+++ src/Mealz/MealBundle/Tests/Controller/MealGuestControllerTest.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mealz\MealBundle\Tests\Controller;
+
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadCategories;
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadDays;
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadDishes;
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadDishVariations;
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadEvents;
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadMeals;
+use App\Mealz\MealBundle\DataFixtures\ORM\LoadWeeks;
+use App\Mealz\MealBundle\Repository\GuestInvitationRepository;
+use App\Mealz\UserBundle\DataFixtures\ORM\LoadRoles;
+use App\Mealz\UserBundle\DataFixtures\ORM\LoadUsers;
+
+class MealGuestControllerTest extends AbstractControllerTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->clearAllTables();
+        $this->loadFixtures([
+            new LoadWeeks(),
+            new LoadDays(),
+            new LoadCategories(),
+            new LoadDishes(),
+            new LoadDishVariations(),
+            new LoadEvents(),
+            new LoadMeals(),
+            new LoadRoles(),
+            new LoadUsers(self::$container->get('security.user_password_encoder.generic')),
+        ]);
+
+        $this->loginAs(self::USER_KITCHEN_STAFF);
+    }
+
+    public function testnewGuestEventInvitation(): void
+    {
+        $guestInvitationRepo = self::$container->get(GuestInvitationRepository::class);
+        $eventParticipation = $this->createFutureEvent();
+        $url = '/event/invitation/' . $eventParticipation->getDay()->getId();
+
+        $this->client->request('GET', $url);
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $guestLink = json_decode($this->client->getResponse()->getContent())->url;
+        $prefix = 'http://localhost/guest/event/';
+        $invitationId = str_ireplace($prefix, '', $guestLink);
+
+        $invitation = $guestInvitationRepo->find($invitationId);
+        $this->assertNotNull($invitation);
+        $this->assertEquals(
+            $eventParticipation->getEvent()->getTitle(),
+            $invitation->getDay()->getEvent()->getEvent()->getTitle()
+        );
+    }
+
+    public function testGetEventInvitationData(): void
+    {
+        $guestInvitationRepo = self::$container->get(GuestInvitationRepository::class);
+        $eventParticipation = $this->createFutureEvent();
+        $profile = $this->createProfile('Max', 'Mustermann' . time());
+        $this->persistAndFlushAll([$profile]);
+        $eventInvitation = $guestInvitationRepo->findOrCreateInvitation($profile, $eventParticipation->getDay());
+
+        $this->client->request('GET', '/api/event/invitation/' . $eventInvitation->getId());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $content = json_decode($this->client->getResponse()->getContent());
+        $this->assertEquals(
+            json_encode($eventParticipation->getDay()->getDateTime()),
+            json_encode($content->date)
+        );
+        $this->assertEquals(
+            json_encode($eventParticipation->getDay()->getLockParticipationDateTime()),
+            json_encode($content->lockDate)
+        );
+        $this->assertEquals(
+            $eventParticipation->getDay()->getEvent()->getEvent()->getTitle(),
+            $content->event
+        );
+    }
+
+    public function testJoinEventAsGuest(): void
+    {
+        $guestInvitationRepo = self::$container->get(GuestInvitationRepository::class);
+        $eventParticipation = $this->createFutureEvent();
+        $profile = $this->createProfile('Max', 'Mustermann' . time());
+        $this->persistAndFlushAll([$profile]);
+        $eventInvitation = $guestInvitationRepo->findOrCreateInvitation($profile, $eventParticipation->getDay());
+
+        // with company
+        $this->client->request(
+            'POST',
+            '/api/event/invitation/' . $eventInvitation->getId(),
+            [],
+            [],
+            [],
+            json_encode([
+                'firstName' => 'John',
+                'lastName' => 'Doe',
+                'company' => 'District 9',
+            ])
+        );
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        // without company
+        $this->client->request(
+            'POST',
+            '/api/event/invitation/' . $eventInvitation->getId(),
+            [],
+            [],
+            [],
+            json_encode([
+                'firstName' => 'Jane',
+                'lastName' => 'Doe',
+                'company' => null,
+            ])
+        );
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        // without firstName
+        $this->client->request(
+            'POST',
+            '/api/event/invitation/' . $eventInvitation->getId(),
+            [],
+            [],
+            [],
+            json_encode([
+                'firstName' => null,
+                'lastName' => 'Doe',
+                'company' => 'District 9',
+            ])
+        );
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+    }
+}

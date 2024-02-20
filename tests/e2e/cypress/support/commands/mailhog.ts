daee31e5a7248bee53f4140f7e37cde375295376
MalibusParty <76796838+MalibusParty@users.noreply.github.com>
2024-02-20 10:04:21 +0100

    another potential fix for the mailhog bug

--- tests/e2e/cypress/support/commands/mailhog.ts
+++ tests/e2e/cypress/support/commands/mailhog.ts
@@ -22,29 +22,28 @@ export const visitSettlementLinkFromMail = (times: number = 0) => {
                 const confirmationURL = replaced.split(Cypress.env('baseUrl'))[1];
                 cy.log(`V2:: confimartion URL: ${confirmationURL}`);
                 cy.visit(`${Cypress.env('baseUrl')}${confirmationURL}`);
-                return;
             }
+        } else {
+            cy.log('Checking response of mailhog api v1, because v2 is not responding');
+            cy.request({ url: `${Cypress.env('mailhog_url')}/api/v1/messages?start=0&limit=1`, failOnStatusCode: false }).then((response) => {
+                cy.log(`V1:: Times: ${times}, Response: ${JSON.stringify(response)}`);
+                cy.wait(1000);
+                if (response.status !== 200) {
+                    cy.log('V1:: retrying getting settling mail');
+                    visitSettlementLinkFromMail(times + 1);
+                } else {
+                    const html = response.body;
+                    cy.log(`V1:: Body: ${JSON.stringify(html)}`);
+                    const message = html.messages[0].Snippet;
+                    cy.log(`V1:: message: ${JSON.stringify(message)}`);
+                    const replaced = message.replace(/(\r\n|\n|\r|=)/gm, "");
+                    cy.log(`V1:: Replaced: ${JSON.stringify(replaced)}`);
+                    const confirmationURL = replaced.split(Cypress.env('baseUrl'))[1];
+                    cy.log(`V1:: confimartion URL: ${confirmationURL}`);
+                    cy.visit(`${Cypress.env('baseUrl')}${confirmationURL}`);
+                }
+            });
         }
     });
 
-    cy.log('Checking response of mailhog api v1, because v2 is not responding');
-    cy.request({ url: `${Cypress.env('mailhog_url')}/api/v1/messages?start=0&limit=1`, failOnStatusCode: false }).then((response) => {
-        cy.log(`V1:: Times: ${times}, Response: ${JSON.stringify(response)}`);
-        cy.wait(1000);
-        if (response.status !== 200) {
-            cy.log('V1:: retrying getting settling mail');
-            visitSettlementLinkFromMail(times + 1);
-        } else {
-            const html = response.body;
-            cy.log(`V1:: Body: ${JSON.stringify(html)}`);
-            const message = html.messages[0].Snippet;
-            cy.log(`V1:: message: ${JSON.stringify(message)}`);
-            const replaced = message.replace(/(\r\n|\n|\r|=)/gm, "");
-            cy.log(`V1:: Replaced: ${JSON.stringify(replaced)}`);
-            const confirmationURL = replaced.split(Cypress.env('baseUrl'))[1];
-            cy.log(`V1:: confimartion URL: ${confirmationURL}`);
-            cy.visit(`${Cypress.env('baseUrl')}${confirmationURL}`);
-            return;
-        }
-    });
 }

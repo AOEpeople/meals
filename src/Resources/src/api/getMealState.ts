fcd94302ab49dfc65186d1e67ccafcb83fd8f533
MalibusParty <76796838+MalibusParty@users.noreply.github.com>
2024-03-27 14:28:26 +0100

    fixed prettier linting

--- src/Resources/src/api/getMealState.ts
+++ src/Resources/src/api/getMealState.ts
@@ -4,11 +4,7 @@ import useApi from './api';
  * Gets the mealState for a specified Meal
  */
 export default async function getMealState(mealId: number) {
-    const {
-        error,
-        response: mealstate,
-        request
-    } = useApi<string>('GET', `api/mealstate/${mealId}`);
+    const { error, response: mealstate, request } = useApi<string>('GET', `api/mealstate/${mealId}`);
 
     await request();
 

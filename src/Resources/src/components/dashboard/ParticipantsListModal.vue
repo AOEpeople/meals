9e5b3751d421a97e045fb18dfbe74c0366991df4
AYAxDesign <aya.akutsu@aoe.com>
2024-06-18 13:21:54 +0200

    fixed ParticipantsListModal width

--- src/Resources/src/components/dashboard/ParticipantsListModal.vue
+++ src/Resources/src/components/dashboard/ParticipantsListModal.vue
@@ -6,11 +6,12 @@
   >
     <div class="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.1)] p-4">
       <DialogPanel
-      class="day-shadow relative inset-0 mx-auto h-auto w-max overflow-auto rounded-lg bg-white px-4 pb-4 pt-2.5 text-left drop-shadow-2xl"
+        class="day-shadow relative inset-0 mx-auto h-auto w-96 overflow-auto rounded-lg bg-white px-4 pb-4 pt-2.5 text-left drop-shadow-2xl sm:w-max"
       >
+
       <IconCancel
       :btn-text="t('combiModal.close')"
-      class="absolute right-4 top-4 z-10 flex-1 cursor-pointer"
+      class="absolute right-4 top-5 z-10 flex-1 cursor-pointer"
       @click="closeParticipantsModal(false)"
     />
         <ParticipantsListByDay
@@ -26,8 +27,8 @@
 <script setup lang="ts">
 import { Dialog, DialogPanel } from '@headlessui/vue';
 import { useI18n } from 'vue-i18n';
-import ParticipantsListByDay from '../participations/ParticipantsListByDay.vue';
 import IconCancel from '../misc/IconCancel.vue';
+import ParticipantsListByDay from '../participations/ParticipantsListByDay.vue';
 
 const { t } = useI18n();
 

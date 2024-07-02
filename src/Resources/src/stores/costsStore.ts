3dbc88aa343cef526fd78a708ebb1eae232f36ec
Felix Ruf <felix.ruf@aoe.com>
2024-07-02 16:05:39 +0200

    fixed a bug that caused costs not to be visibly updated if cash was put into the account

--- src/Resources/src/stores/costsStore.ts
+++ src/Resources/src/stores/costsStore.ts
@@ -21,7 +21,7 @@ interface UserCost {
     name: string;
     firstName: string;
     hidden: boolean;
-    costs: Dictionary<number>;
+    costs: Dictionary<string>;
 }
 
 interface ICostsState extends ICosts {
@@ -150,7 +150,7 @@ export function useCosts() {
                     : 'Error on sending settlement';
         } else if (typeof response.value === 'number') {
             CostsState.error = '';
-            CostsState.users[username].costs['total'] += response.value;
+            CostsState.users[username].costs['total'] = String(parseFloat(CostsState.users[username].costs['total']) + response.value);
             sendFlashMessage({
                 type: FlashMessageType.INFO,
                 message: 'costs.cashPayment'

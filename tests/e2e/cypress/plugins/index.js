517d4cbe5cbb5ab545940d9eed715c274d5f5635
Felix Ruf <felix.ruf@aoe.com>
2024-07-04 10:55:44 +0200

    added and configured cypress-split to parallelise the cypress tests

--- tests/e2e/cypress/plugins/index.js
+++ tests/e2e/cypress/plugins/index.js
@@ -1,6 +1,8 @@
-const { initPlugin } = require("cypress-plugin-snapshots/plugin");
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+const cypressSplit = require('cypress-split');
 
 module.exports = (on, config) => {
   initPlugin(on, config);
+  cypressSplit(on, config);
   return config;
 };

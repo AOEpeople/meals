77c0e79cba159d304dee23f42841b5714ce16a08
Iris Olfermann <iris.olfermann@aoe.com>
2024-07-16 16:37:11 +0200

    added second event to the edit week site in the frontend, second event input opens when you input a first one
    changed column name in participant from event to event_participation
    
    altered migrations, added port in docker compose for lunchroulette go project
    
    Task 266725
    - ApiController: eventService injected
    - Refactoring changed event to event_participation in Participant, EventParticipation & Dependencies
    - changed Maria DB version
    
    Teilnehmer verschwinden gefixed
    
    changed lock date for lunchroulette to 16:00 the day before the event
    
    functionality for alert when leaving lunchroulette, still needs styling
    
    finished LunchroulettePopup.vue, it shows up when a user has chosen a meal and is participating in the lunchroulette event and they untick the meal
    
    The Design for the Event Participation Popup was modified to look like the Meals Participants Popup
    
    fixed tests
    
    update token

--- src/Resources/src/components/eventParticipation/EventPopup.vue
+++ src/Resources/src/components/eventParticipation/EventPopup.vue
@@ -5,45 +5,60 @@
       @click="showParticipations = true"
     />
     <PopupModal :isOpen="showParticipations">
-      <div
-        class="grid max-h-[70vh] min-w-[300px] max-w-[310px] grid-cols-1 grid-rows-[auto_minmax(0,1fr)_auto] md:max-w-lg"
-      >
-        <div class="flex h-[48px] flex-row gap-2 rounded-t-lg bg-primary-2 p-2">
-          <span
-            class="grow self-center justify-self-center truncate font-bold uppercase leading-4 tracking-[3px] text-white"
-          >
-            {{ t('dashboard.participations').replace('%EventTitle%', eventTitle) }}
-          </span>
-          <XCircleIcon
-            class="size-8 cursor-pointer self-end text-white transition-transform hover:scale-[120%] hover:text-[#FAFAFA]"
-            @click="showParticipations = false"
+      <!-- maincontainer with padding -->
+      <div class="min-w-[576px] p-[16px] pt-[10px]">
+        <!-- inner container with content in full width -->
+        <div class="w-full">
+          <!-- dialog and close icon -->
+          <div class="flex w-full items-center justify-between gap-4">
+            <!-- DialogTitle -->
+            <DialogTitle
+              class="w-full whitespace-nowrap text-[10px] font-bold uppercase tracking-[1.5px] text-primary sm:inline-block sm:h-6 sm:align-middle sm:text-[12px]"
+            >
+              {{ t('dashboard.participations').replace('%EventTitle%', eventTitle) }}
+            </DialogTitle>
+            <!-- IconCancel -->
+            <IconCancel
+              :btn-text="t('combiModal.close')"
+              class="cursor-pointer"
+              @click="showParticipations = false"
+            />
+          </div>
+          <!-- loading spinner -->
+          <RefreshIcon
+            v-if="isLoading"
+            class="aspect-[1/1] h-[75px] animate-spin-loading place-self-center p-4 text-primary drop-shadow-[0_0_0.35rem_rgba(0,0,0,0.75)]"
           />
-        </div>
-        <RefreshIcon
-          v-if="isLoading"
-          class="aspect-[1/1] h-[75px] animate-spin-loading place-self-center p-4 text-primary drop-shadow-[0_0_0.35rem_rgba(0,0,0,0.75)]"
-        />
-        <ul
-          v-else-if="participations.length > 0"
-          class="overflow-y-auto p-4"
-        >
-          <li
-            v-for="(participation, index) in participations"
-            :key="`${String(participation)}_${index}`"
-            class="border-b-2 p-2 last:border-b-0"
+          <!-- table with participants -->
+          <table
+            v-else-if="participations.length > 0"
+            class="max-h-[300px] w-full overflow-y-auto"
           >
-            {{ String(participation) }}
-          </li>
-        </ul>
-        <span
-          v-else
-          class="p-4"
-        >
-          {{ t('dashboard.noParticipants') }}
-        </span>
-        <span class="w-full border-t-2 p-2 text-center font-bold">
-          {{ t('dashboard.participationCount').replace('%count%', participations.length.toString()) }}
-        </span>
+            <tbody>
+              <template
+                v-for="(participation, index) in participations"
+                :key="`${String(participation)}_${index}`"
+              >
+                <tr :class="[index === 0 ? 'border-t border-gray-300' : 'border-b border-gray-200', 'border-b']">
+                  <td
+                    class="h-6 w-full whitespace-nowrap py-1 text-[12px] font-light text-primary"
+                    data-cy="event-participant"
+                  >
+                    {{ String(participation) }}
+                  </td>
+                </tr>
+              </template>
+            </tbody>
+          </table>
+
+          <!-- message in case there are no participants -->
+          <span
+            v-else
+            class="p-4"
+          >
+            {{ t('dashboard.noParticipants') }}
+          </span>
+        </div>
       </div>
     </PopupModal>
   </div>
@@ -51,9 +66,10 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue';
+import { DialogTitle } from '@headlessui/vue';
+import IconCancel from '../misc/IconCancel.vue';
 import PopupModal from '../misc/PopupModal.vue';
 import { InformationCircleIcon, RefreshIcon } from '@heroicons/vue/outline';
-import { XCircleIcon } from '@heroicons/vue/solid';
 import { useI18n } from 'vue-i18n';
 import { useEvents } from '@/stores/eventsStore';
 

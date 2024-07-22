5757345df7d4549a185695d27ae0431e049e5618
Iris Olfermann <iris.olfermann@aoe.com>
2024-07-22 10:44:36 +0200

    made the design of the event selection responsive

--- src/Resources/src/components/menu/MenuDay.vue
+++ src/Resources/src/components/menu/MenuDay.vue
@@ -1,9 +1,11 @@
 <template>
   <div
-    class="day-shadow group grid grid-cols-[24px_minmax(0,1fr)_58px] grid-rows-4 rounded-lg border-0 border-none bg-white text-center align-middle sm:grid-cols-[24px_minmax(0,1fr)_72px]"
+    class="day-shadow group grid grid-cols-[24px_minmax(0,1fr)_58px] rounded-lg border-0 border-none bg-white text-center align-middle sm:grid-cols-[24px_minmax(0,1fr)_72px]"
+    :class="[selectedEventOne ? 'grid-rows-4' : 'grid-rows-3']"
   >
     <div
-      class="col-start-1 row-span-3 row-start-1 grid w-[24px] grid-rows-[24px_minmax(0,1fr)_24px] justify-center rounded-l-lg bg-primary-2 py-1"
+      class="col-start-1  row-start-1 grid w-[24px] grid-rows-[24px_minmax(0,1fr)_24px] justify-center rounded-l-lg bg-primary-2 py-1"
+      :class="[selectedEventOne ? 'row-span-4' : 'row-span-3']"
     >
       <Popover
         :translate-x-min="'0%'"
@@ -53,7 +55,7 @@
       v-model="selectedEventTwo"
       class="col-start-2 row-span-1 row-start-4 px-2 py-[12px] md:px-4"
     />
-    <div class="col-start-3 row-span-3 row-start-1 grid items-center rounded-r-lg border-l-2 sm:w-[72px]">
+    <div class="col-start-3 row-span-4 row-start-1 grid items-center rounded-r-lg border-l-2 sm:w-[72px]">
       <Switch
         :sr="t('menu.enableDay')"
         :initial="modelValue.enabled"

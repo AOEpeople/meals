8a696f8b0eef0ec4426dfa0c7d0627e8dd7bf159
Iris Olfermann <iris.olfermann@aoe.com>
2024-07-30 10:32:20 +0200

    updated api calls for events now including event id, added getEventIdBySlug Method to fetch eventId by its title

--- src/Resources/src/api/getEventParticipants.ts
+++ src/Resources/src/api/getEventParticipants.ts
@@ -1,8 +1,8 @@
 import useApi from './api';
 import { type IMessage } from '@/interfaces/IMessage';
 
-export default async function getEventParticipants(date: string) {
-    const { error, response, request } = useApi<string[] | IMessage>('GET', `api/participations/event/${date}`);
+export default async function getEventParticipants(date: string, eventId: number) {
+    const { error, response, request } = useApi<string[] | IMessage>('GET', `api/participations/event/${date}/${eventId}`);
 
     await request();
 

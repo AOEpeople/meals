0d5c7f90d598dc93ae398ce20070088f68715349
Felix Ruf <felix.ruf@aoe.com>
2024-08-02 13:27:14 +0200

    adjusted github actions pipeline to use vitest and vue-tsc

--- src/Resources/src/api/getPrintParticipations.ts
+++ src/Resources/src/api/getPrintParticipations.ts
@@ -1,5 +1,5 @@
-import useApi from "@/api/api";
-import type { IParticipationsState } from "./getShowParticipations";
+import useApi from '@/api/api';
+import type { IParticipationsState } from './getShowParticipations';
 
 export default async function getPrintParticipations() {
     const { response, request, error } = useApi<IParticipationsState>('GET', '/api/print/participations');

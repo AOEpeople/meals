7972178f3a44e2184e10140d6ced08e34d701e4d
Iris Olfermann <iris.olfermann@aoe.com>
2024-08-02 15:42:47 +0200

    updated event id in data set migration

--- migrations/Version20240722142949.php
+++ migrations/Version20240722142949.php
@@ -21,10 +21,13 @@ final class Version20240722142949 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE day DROP event_id');
+        $this->addSql('ALTER TABLE event_participation DROP INDEX UNIQ_8F0C52E3E5A02990');
+        $this->addSql('CREATE UNIQUE INDEX unique_day_event ON event_participation (day, event)');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE day ADD event_id INT DEFAULT NULL');
+        $this->addSql('DROP INDEX unique_day_event ON event_participation');
     }
 }

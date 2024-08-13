d7e48eeaeaf96a110911485a471304bc8bd79194
Felix Ruf <felix.ruf@aoe.com>
2024-08-13 11:53:12 +0200

    removed tcpdf, because it is no longer used to create pdfs and has a vulnerability

--- src/Mealz/AccountingBundle/Controller/AccountingBookController.php
+++ src/Mealz/AccountingBundle/Controller/AccountingBookController.php
@@ -8,12 +8,9 @@ use App\Mealz\AccountingBundle\Repository\TransactionRepositoryInterface;
 use App\Mealz\MealBundle\Controller\BaseController;
 use DateTime;
 use Exception;
-use Qipsius\TCPDFBundle\Controller\TCPDFController;
-use ReflectionException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[IsGranted('ROLE_KITCHEN_STAFF')]
 class AccountingBookController extends BaseController
@@ -96,62 +93,5 @@ class AccountingBookController extends BaseController
         ];
 
         return new JsonResponse($response, Response::HTTP_OK);
-    }
-
-    /**
-     * Export transaction table as PDF for finance staff.
-     *
-     * @throws ReflectionException
-     * @throws Exception
-     */
-    public function exportPDF(
-        ?string $dateRange,
-        TCPDFController $pdfGen,
-        TransactionRepositoryInterface $transactionRepo,
-        TranslatorInterface $translator
-    ): Response {
-        // Get date range set with date range picker by user
-        $dateRange = str_replace('-', '/', $dateRange);
-        $dateRangeArray = explode('&', $dateRange);
-        $minDate = new DateTime($dateRangeArray[0]);
-        $maxDate = new DateTime($dateRangeArray[1]);
-
-        $heading = $minDate->format('d.m.') . ' - ' . $maxDate->format('d.m.Y');
-        $transactions = $transactionRepo->findAllTransactionsInDateRange($minDate, $maxDate);
-
-        // Create PDF file
-        $pdf = $pdfGen->create(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-        $pdf->setHeaderData('', 0, '', '', [0, 0, 0], [255, 255, 255]);
-        $pdf->setPrintHeader(false);
-        $pdf->setPrintFooter(false);
-        $pdf->AddPage();
-
-        $filename = $translator->trans('payment.transaction_history.finances.pdf') . '-' . $minDate->format('d.m.Y') . '-' . $maxDate->format('d.m.Y');
-        $pdf->SetTitle($filename);
-
-        $cssFile = file_get_contents(__DIR__ . '/../Resources/css/transaction-export.css');
-
-        $includeCSS = '<style>' . $cssFile . '</style>';
-
-        $html = $this->renderView('MealzAccountingBundle:Accounting/Finance:print_finances.html.twig', [
-            'headingFirst' => null,
-            'heading' => $heading,
-            'transactionsFirst' => null,
-            'transactions' => $transactions,
-            'minDate' => $minDate->format('m/d/Y'),
-            'maxDate' => $maxDate->format('m/d/Y'),
-        ]);
-
-        $pdf->writeHTML($includeCSS . $html);
-
-        $content = $pdf->Output($filename . '.pdf', 'S');
-        $now = gmdate('D, d M Y H:i:s') . ' GMT';
-
-        return new Response($content, Response::HTTP_OK, [
-            'Content-Type' => 'application/pdf',
-            'Expires' => $now,
-            'Last-Modified' => $now,
-            'Content-Disposition' => 'inline; filename="' . basename($filename) . '"',
-        ]);
     }
 }

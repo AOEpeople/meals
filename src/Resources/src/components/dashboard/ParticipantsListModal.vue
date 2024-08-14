05a71a464184940a2c976552ececc99bccd4e48f
Felix Ruf <felix.ruf@aoe.com>
2024-08-14 10:20:03 +0200

    fixed missing shadows by removing global css

--- src/Resources/src/components/dashboard/ParticipantsListModal.vue
+++ src/Resources/src/components/dashboard/ParticipantsListModal.vue
@@ -6,7 +6,7 @@
   >
     <div class="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.1)] p-4">
       <DialogPanel
-        class="day-shadow relative inset-0 mx-auto h-auto w-96 overflow-auto rounded-lg bg-white px-4 pb-4 pt-2.5 text-left drop-shadow-2xl sm:w-max"
+        class="relative inset-0 mx-auto h-auto w-96 overflow-auto rounded-lg bg-white px-4 pb-4 pt-2.5 text-left shadow-day drop-shadow-2xl sm:w-max"
       >
         <IconCancel
           :btn-text="t('combiModal.close')"

0773fec23aa72d7b9bd05248aba4176954902136
felix.ruf <felix.ruf@aoe.com>
2025-05-16 10:09:52 +0200

    add ordering of weeks by startdate to prevent false ordering by week id

--- src/Resources/src/components/dashboard/DashboardWeekAll.vue
+++ src/Resources/src/components/dashboard/DashboardWeekAll.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="inline-flex divide-x-2 print:block print:h-screen">
     <div
-      v-for="(weekID, index) in Object.keys(weeks)"
+      v-for="(weekID, index) in weekIdsSortedByDate"
       :key="index"
       class="w-[585px] flex-initial print:mx-auto print:w-full print:last:hidden"
     >
@@ -17,8 +17,22 @@
 import { type Dictionary } from '@/types/types';
 import WeekComp from './Week.vue';
 import { type Week } from '@/api/getDashboardData';
+import { computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   weeks: Dictionary<Week>;
 }>();
+
+const weekIdsSortedByDate = computed(() =>
+  Object.keys(props.weeks).sort((a, b) => {
+    const startDateA = new Date(props.weeks[a].startDate.date);
+    const startDateB = new Date(props.weeks[b].startDate.date);
+
+    if (startDateA > startDateB) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+);
 </script>

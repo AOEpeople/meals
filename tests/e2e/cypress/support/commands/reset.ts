8bc615bb0afe992ab9b4cf515711433a466b8ed8
Martin Tepper <martin.tepper@aoe.com>
2025-09-24 14:49:21 +0200

    use cypress 15.3.0

--- tests/e2e/cypress/support/commands/reset.ts
+++ tests/e2e/cypress/support/commands/reset.ts
@@ -1,7 +1,7 @@
 export const resetDB = () => {
     if (Cypress.env('ddev_test')) {
-        cy.exec('cd ./../.. && make load-testdata', { log: true, failOnNonZeroExit: true }).its('code').should('eq', 0);
+        cy.exec('cd ./../.. && make load-testdata', { log: true, failOnNonZeroExit: true }).its('exitCode').should('eq', 0);
     } else {
-        cy.exec('docker exec app bin/console doctrine:fixtures:load -n', { log: true, failOnNonZeroExit: true }).its('code').should('eq', 0);
+        cy.exec('docker exec app bin/console doctrine:fixtures:load -n', { log: true, failOnNonZeroExit: true }).its('exitCode').should('eq', 0);
     }
 }

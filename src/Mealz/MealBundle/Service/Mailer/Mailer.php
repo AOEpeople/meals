34ae404db9f1b514bc6e48ba92641c96d8babe63
Thomas Schenk <Thomas.Schenk@aoe.com>
2025-10-21 08:51:58 +0200

    Migration to MS OAuth SMTP

--- src/Mealz/MealBundle/Service/Mailer/Mailer.php
+++ src/Mealz/MealBundle/Service/Mailer/Mailer.php
@@ -4,22 +4,21 @@ declare(strict_types=1);
 
 namespace App\Mealz\MealBundle\Service\Mailer;
 
+use Exception;
 use Override;
+use PHPMailer\PHPMailer\PHPMailer;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Mailer\MailerInterface as SymfonyMailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 
 final class Mailer implements MailerInterface
 {
     private string $senderEmail;
 
     private LoggerInterface $logger;
-    private SymfonyMailerInterface $mailer;
+
+    private OAuthMailer $mailer;
 
     public function __construct(
-        SymfonyMailerInterface $mailer,
+        OAuthMailer $mailer,
         LoggerInterface $logger,
         string $senderEmail
     ) {
@@ -31,20 +30,20 @@ final class Mailer implements MailerInterface
     #[Override]
     public function send(string $recipient, string $subject, string $content, bool $isHTML = false): void
     {
-        $email = (new Email())
-            ->from(Address::create($this->senderEmail))
-            ->to($recipient)
-            ->subject($subject)
-            ->text(strip_tags($content))
-            ->html($content);
-
-        if ($isHTML) {
-            $email->html($content);
-        }
-
         try {
-            $this->mailer->send($email);
-        } catch (TransportExceptionInterface $e) {
+            $this->mailer->setFrom($this->senderEmail);
+            $this->mailer->addAddress($recipient);
+
+            $this->mailer->Subject = $subject;
+            $this->mailer->CharSet = PHPMailer::CHARSET_UTF8;
+            $this->mailer->Body = strip_tags($content);
+            $this->mailer->msgHTML($content);
+            $this->mailer->isHTML($isHTML);
+
+            if (!$this->mailer->send()) {
+                $this->logger->error('email send error: ' . $this->mailer->ErrorInfo);
+            }
+        } catch (Exception $e) {
             $this->logger->error('email send error', [
                 'exception' => $e->getMessage(),
                 'trace' => $e->getTraceAsString(),

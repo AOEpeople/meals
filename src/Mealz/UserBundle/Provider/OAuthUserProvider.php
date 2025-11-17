fb67ae0674c0585ee27f2f46550bd455f5fb03bb
Martin Tepper <martin.tepper@aoe.com>
2025-11-17 14:50:44 +0100

    use a new field for sso identifier

--- src/Mealz/UserBundle/Provider/OAuthUserProvider.php
+++ src/Mealz/UserBundle/Provider/OAuthUserProvider.php
@@ -82,7 +82,6 @@ final class OAuthUserProvider implements UserProviderInterface, OAuthAwareUserPr
         } catch (UserNotFoundException $e) {
             // Find user by username; set IdpUser ID
             $user = $this->entityManager->getRepository(Profile::class)->findOneBy(['username' => $username]);
-
             if ($user instanceof UserInterface) {
                 $user->setSsoId($idpUserId);
 
@@ -91,7 +90,6 @@ final class OAuthUserProvider implements UserProviderInterface, OAuthAwareUserPr
 
                 $user = $this->loadUserByIdentifier($idpUserId);
             }
-        }
 
         // Create user
         if (false === ($user instanceof UserInterface)) {

c889fb3cb6fd2e94e0cfab42d08251b168c1e610
Aria Gholami <aria.gholami@aoe.com>
2025-12-10 09:53:58 +0100

    feat: add migrations for referencing prices to meals
    #272074

---
+++ migrations/Version20251209102354.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MariaDBPlatform;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20251209102354 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add price reference between meal and price table.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortOnIncompatibleDB();
+
+        $this->addSql('ALTER TABLE meal ADD COLUMN price_id INT NOT NULL;');
+        $this->addSql('UPDATE meal SET price_id = YEAR(dateTime);');
+        $this->addSql('ALTER TABLE meal ADD CONSTRAINT fk_meal_price FOREIGN KEY (price_id) REFERENCES price(year);');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortOnIncompatibleDB();
+
+        $this->addSql('ALTER TABLE meal DROP FOREIGN KEY fk_meal_price;');
+        $this->addSql('ALTER TABLE meal DROP COLUMN price;');
+    }
+
+    /**
+     * @throws Exception
+     */
+    function abortOnIncompatibleDB(): void
+    {
+        $currPlatform = $this->connection->getDatabasePlatform();
+        $this->abortIf(
+            !($currPlatform instanceof MySQLPlatform || $currPlatform instanceof MariaDBPlatform),
+            'Migration can only be executed safely on \'mysql\' or \'maria-db\'.'
+        );
+    }
+}

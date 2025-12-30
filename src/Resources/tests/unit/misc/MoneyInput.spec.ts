b7b262d5555ccb22b05146a45eb3ed0627f205ff
Aria Gholami <aria.gholami@aoe.com>
2025-12-30 14:07:31 +0100

    feat(moneyInput): Add test for money converting
    #272351

---
+++ src/Resources/tests/unit/misc/MoneyInput.spec.ts
@@ -0,0 +1,61 @@
+import { describe, it, expect, beforeEach} from 'vitest';
+import MoneyInput from '../../../src/components/misc/MoneyInput.vue';
+import {mount} from '@vue/test-utils';
+describe('Test MoneyInput', () => {
+    it('should input number and convert to money', async () => {
+        const wrapper = mount(MoneyInput, {
+            props: {
+                modelValue: 1234.567,
+            },
+        });
+
+        const input = wrapper.get('input');
+
+        expect((input.element as HTMLInputElement).value).toBe('1.234,57');
+
+        await input.trigger('focus');
+        expect((input.element as HTMLInputElement).value).toBe('1234.567');
+
+        await input.trigger('blur');
+        expect((input.element as HTMLInputElement).value).toBe('1.234,57');
+    });
+
+    it('should input number with 30 cents and convert to money', async () => {
+        const wrapper = mount(MoneyInput, {
+            props: {
+                modelValue: 12.3,
+            },
+        });
+
+        const input = wrapper.get('input');
+        await input.setValue('12,3');
+
+        expect((input.element as HTMLInputElement).value).toBe('12,3');
+
+        await input.trigger('focus');
+        expect((input.element as HTMLInputElement).value).toBe('12.3');
+
+        await input.trigger('blur');
+        expect((input.element as HTMLInputElement).value).toBe('12,30');
+    });
+
+    it('should input negative number and not convert to money', async () => {
+        const wrapper = mount(MoneyInput, {
+            props: {
+                modelValue: 1234.5,
+            },
+        });
+
+        const input = wrapper.get('input');
+        await input.setValue('-1234.5');
+        expect(wrapper.emitted("update:modelValue")).toBeFalsy();
+
+        expect((input.element as HTMLInputElement).value).toBe('-1234.5');
+
+        await input.trigger('focus');
+        expect((input.element as HTMLInputElement).value).toBe('1234.5');
+
+        await input.trigger('blur');
+        expect((input.element as HTMLInputElement).value).toBe('1.234,50');
+    });
+});

b766437c9b42547280cb533868340ad6f4534255
Aria Gholami <aria.gholami@aoe.com>
2025-12-30 16:59:15 +0100

    feat(navBar): Add space between balance and navigation
    #271656

--- src/Resources/src/components/NavBar.vue
+++ src/Resources/src/components/NavBar.vue
@@ -74,7 +74,7 @@
           </div>
           <div
             v-if="isAuthenticated && !isShowParticipations"
-            class="col-span-4 flex space-x-4 justify-self-end"
+            class="col-span-4 flex space-x-4 justify-self-end -mr-2"
           >
             <div class="hidden space-x-2 self-center text-right xl:inline-block">
               <Icons

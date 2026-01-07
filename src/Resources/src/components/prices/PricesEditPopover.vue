1dd34feb5340af3b1f088e29ad68aaa644f002bd
Aria Gholami <aria.gholami@aoe.com>
2026-01-07 08:58:28 +0100

    feat(prices): change styling for input error message general

--- src/Resources/src/components/prices/PricesEditPopover.vue
+++ src/Resources/src/components/prices/PricesEditPopover.vue
@@ -29,7 +29,7 @@
 
       <p
         v-if="errors.general"
-        class="text-red-600 mb-3 text-sm"
+        class="text-[#DC2626] font-medium mb-3 text-sm"
       >
         {{ errors.general }}
       </p>

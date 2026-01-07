4809f18b7615fe3c6f3196aee16c20f0762c899a
Aria Gholami <aria.gholami@aoe.com>
2026-01-07 09:03:14 +0100

    refactor(prices): remove unnecessary beforeEach import

--- src/Resources/tests/unit/misc/MoneyInput.spec.ts
+++ src/Resources/tests/unit/misc/MoneyInput.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach} from 'vitest';
+import { describe, it, expect} from 'vitest';
 import MoneyInput from '../../../src/components/misc/MoneyInput.vue';
 import {mount} from '@vue/test-utils';
 describe('Test MoneyInput', () => {

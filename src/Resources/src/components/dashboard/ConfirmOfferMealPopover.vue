78ba0fe22e54d642e0b9d7aea534382a36b0ebba
Aria Gholami <aria.gholami@aoe.com>
2026-01-19 09:13:19 +0100

    feat: display ConfirmOfferMealPopover before offered meal is taken over
    #272718

---
+++ src/Resources/src/components/dashboard/ConfirmOfferMealPopover.vue
@@ -0,0 +1,32 @@
+<template>
+  <Modal
+    :show="show"
+    @close="$emit('close')"
+  >
+    <div class="w-[300px] rounded-lg bg-white p-6 shadow-lg md:w-[400px]">
+      <h3 class="mb-4 text-lg font-semibold">{{ t('confirmOfferMeal.popover.title') }}</h3>
+      <button
+        type="button"
+        class="btn-primary inline-flex w-full justify-center"
+        @click="$emit('confirm')"
+      >
+        {{ t('confirmOfferMeal.popover.button') }}
+      </button>
+    </div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import Modal from '@/components/misc/Modal.vue';
+const { t } = useI18n();
+
+defineProps<{
+  show: boolean;
+}>();
+
+defineEmits<{
+  close: [];
+  confirm: [];
+}>();
+</script>
